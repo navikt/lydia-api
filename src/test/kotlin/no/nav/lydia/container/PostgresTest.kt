49b06be21296ae146468368ec7601b2b1d37e798
Ulrik Antoniussen Halmøy <ulrikhalmoy@gmail.com>
2022-01-12 14:05:47 +0100

    Bruk postgres 12 fordi det er det nais støtter
    Co-authored-by: Christian Klem <christian.klem@nav.no>
    Co-authored-by: Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>

--- src/test/kotlin/no/nav/lydia/container/PostgresTest.kt
+++ src/test/kotlin/no/nav/lydia/container/PostgresTest.kt
@@ -11,7 +11,7 @@ import kotlin.test.assertEquals
 
 class PostgresTest {
     private val postgresContainer: PostgreSQLContainer<*> =
-        PostgreSQLContainer("postgres:latest").waitingFor(HostPortWaitStrategy()).apply {
+        PostgreSQLContainer("postgres:12").waitingFor(HostPortWaitStrategy()).apply {
             start()
         }
 
