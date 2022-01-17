6c2920ca3ca98849ff45f1e560c09cfe9a61d457
Stefan A. Engelien <stefan.andreas.engelien@nav.no>
2022-01-17 11:15:30 +0100

    Tester at databasen blir satt opp skikkelig
    Co-authored-by: Christian Klem <christian.klem@nav.no>

--- src/main/kotlin/no/nav/lydia/NaisEnvironment.kt
+++ src/main/kotlin/no/nav/lydia/NaisEnvironment.kt
@@ -19,39 +19,23 @@ enum class NaisCluster(val value: String) {
 }
 
 class NaisEnvironment(val naisCluster: NaisCluster) {
-    val database = Database(naisCluster)
+    val database = Database()
+}
 
+class Database {
+    val jdbcUrl: String
+    val username: String
+    val password: String
+    val name: String
+
+    init {
+        val nais_db_prefix = "NAIS_DATABASE_LYDIA_API_LYDIA_API_DB_"
+        name = getEnvVar("${nais_db_prefix}DATABASE")
+        username = getEnvVar("${nais_db_prefix}USERNAME")
+        password = getEnvVar("${nais_db_prefix}PASSWORD")
+        jdbcUrl = getEnvVar("${nais_db_prefix}URL")
     }
-
-    class Database(naisCluster: NaisCluster) {
-        private val host: String
-        private val port: String
-        val username: String
-        val password: String
-        val name: String
-        val jdbcUrl: String
-
-        init {
-            val nais_db_prefix = "NAIS_DATABASE_LYDIA_API_LYDIA_API_DB_"
-            when (naisCluster) {
-                NaisCluster.DEV, NaisCluster.PROD -> {
-                    host = getEnvVar("${nais_db_prefix}HOST")
-                    port = getEnvVar("${nais_db_prefix}PORT")
-                    name = getEnvVar("${nais_db_prefix}DATABASE")
-                    username = getEnvVar("${nais_db_prefix}USERNAME")
-                    password = getEnvVar("${nais_db_prefix}PASSWORD")
-                }
-                NaisCluster.LOCAL -> {
-                    host = getEnvVar(varName = "${nais_db_prefix}HOST", defaultValue = "localhost")
-                    port = "5432"
-                    name =  getEnvVar(varName = "POSTGRES_DB") // Hentes fra PostgreSQLContainer
-                    username = getEnvVar(varName = "POSTGRES_USER")
-                    password = getEnvVar(varName = "POSTGRES_PASSWORD")
-                }
-            }
-            jdbcUrl = String.format("jdbc:postgresql://%s:%s/%s", host, port, name)
-        }
-    }
+}
 
 fun getEnvVar(varName: String, defaultValue: String? = null) =
     System.getenv(varName) ?: defaultValue ?: throw RuntimeException("Missing required variable $varName")
