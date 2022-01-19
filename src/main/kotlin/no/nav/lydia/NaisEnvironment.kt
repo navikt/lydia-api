3d16070dacd07776147060e3802564ed1da39f32
Stefan A. Engelien <stefan.andreas.engelien@nav.no>
2022-01-19 16:12:57 +0100

    Setter opp påkrevd azure-config for komponenttesten
    Co-authored-by: Tanet Trimas <tanet.trimas@nav.no>
    Co-authored-by: Christian Klem <christian.klem@nav.no>

--- src/main/kotlin/no/nav/lydia/NaisEnvironment.kt
+++ src/main/kotlin/no/nav/lydia/NaisEnvironment.kt
@@ -2,37 +2,25 @@ package no.nav.lydia
 
 import java.net.URL
 
-class NaisEnvironment() {
-    val database = Database()
-    val security = Security()
-}
+class NaisEnvironment(
+    val database: Database = Database(),
+    val security: Security = Security()
+)
 
-class Database {
-    val host: String
-    val port: String
-    val username: String
-    val password: String
-    val name: String
+class Database(
+    val host: String = getEnvVar("NAIS_DATABASE_LYDIA_API_LYDIA_API_DB_HOST"),
+    val port: String = getEnvVar("NAIS_DATABASE_LYDIA_API_LYDIA_API_DB_PORT"),
+    val username: String = getEnvVar("NAIS_DATABASE_LYDIA_API_LYDIA_API_DB_DATABASE"),
+    val password: String = getEnvVar("NAIS_DATABASE_LYDIA_API_LYDIA_API_DB_USERNAME"),
+    val name: String = getEnvVar("NAIS_DATABASE_LYDIA_API_LYDIA_API_DB_PASSWORD")
+)
 
-    init {
-        val naisDbPrefix = "NAIS_DATABASE_LYDIA_API_LYDIA_API_DB_"
-        host = getEnvVar("${naisDbPrefix}HOST")
-        port = getEnvVar("${naisDbPrefix}PORT")
-        name = getEnvVar("${naisDbPrefix}DATABASE")
-        username = getEnvVar("${naisDbPrefix}USERNAME")
-        password = getEnvVar("${naisDbPrefix}PASSWORD")
-    }
-}
-
-class Security {
-    class AzureConfig {
-        val audience: String = getEnvVar("AZURE_APP_CLIENT_ID")
-        val jwksUri: URL = URL(getEnvVar("AZURE_OPENID_CONFIG_JWKS_URI"))
+class Security(val azureConfig: AzureConfig = AzureConfig())
+    class AzureConfig(
+        val audience: String = getEnvVar("AZURE_APP_CLIENT_ID"),
+        val jwksUri: URL = URL(getEnvVar("AZURE_OPENID_CONFIG_JWKS_URI")),
         val issuer: String = getEnvVar("AZURE_OPENID_CONFIG_ISSUER")
-    }
-
-    val azureConfig = AzureConfig()
-}
+    )
 
 fun getEnvVar(varName: String, defaultValue: String? = null) =
     System.getenv(varName) ?: defaultValue ?: throw RuntimeException("Missing required variable $varName")
--- src/test/kotlin/no/nav/lydia/AppTest.kt
+++ src/test/kotlin/no/nav/lydia/AppTest.kt
@@ -1,15 +1,35 @@
 package no.nav.lydia
 
-import io.ktor.application.*
 import io.ktor.http.*
 import io.ktor.server.testing.*
+import java.net.URL
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class AppTest {
-    @Test fun appHasAGreeting() = withTestApplication(Application::lydiaBackend) {
-        with(handleRequest(HttpMethod.Get, "/isAlive")) {
-            assertEquals("OK", response.content)
+    private val naisEnv = NaisEnvironment(
+        database = Database(
+            host = "",
+            port = "",
+            username = "",
+            password = "",
+            name = ""
+        ), security = Security(
+            AzureConfig(
+                audience = "",
+                jwksUri = URL("https://www.example.com"),
+                issuer = ""
+            )
+        )
+    )
+
+    @Test
+    fun appHasAGreeting() {
+        withTestApplication({ lydiaBackend(naisEnv) }) {
+            with(handleRequest(HttpMethod.Get, "/isAlive")) {
+                assertEquals("OK", response.content)
+            }
         }
     }
+
 }
