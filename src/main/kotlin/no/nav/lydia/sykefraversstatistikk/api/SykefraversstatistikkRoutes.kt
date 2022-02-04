fa52037484ce9db8ed9f03dbdbc388e40f12f01f
Ulrik Antoniussen Halmøy <ulrikhalmoy@gmail.com>
2022-02-04 11:40:44 +0100

    WIP: filtrer på fylke i søk på virksomheter
    Co-authored-by: Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>

--- src/main/kotlin/no/nav/lydia/sykefraversstatistikk/api/SykefraversstatistikkRoutes.kt
+++ src/main/kotlin/no/nav/lydia/sykefraversstatistikk/api/SykefraversstatistikkRoutes.kt
@@ -1,16 +1,32 @@
 package no.nav.lydia.sykefraversstatistikk.api
 
 import io.ktor.application.*
+import io.ktor.http.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import no.nav.lydia.sykefraversstatistikk.api.geografi.GeografiService
+import no.nav.lydia.virksomhet.VirksomhetService
 
 val SYKEFRAVERSSTATISTIKK_PATH = "sykefraversstatistikk"
 val FILTERVERDIER_PATH = "filterverdier"
 
 fun Route.sykefraversstatistikk() {
+    val virksomhetService = VirksomhetService()
+    val geografiService = GeografiService()
+
+
     get("$SYKEFRAVERSSTATISTIKK_PATH/") {
-        call.respond("OK")
+        val fylkesNummerISøk = call.request.queryParameters["fylke"]?.split(",")?.toSet() ?: emptySet()
+
+        val alleFylker = geografiService.hentFylker().associateBy{ it.nummer }
+        val virksomheter = virksomhetService.hentVirksomheterFraFylkesnummer(fylkesNummerISøk.filter{
+            fylkesnummer -> alleFylker.containsKey(fylkesnummer)
+        })
+        call.respond(virksomheter)
+
+//      /api?fylke=Viken,Vestland&
+//
+//
     }
 
     get("$SYKEFRAVERSSTATISTIKK_PATH/{orgnummer}") {
@@ -18,6 +34,6 @@ fun Route.sykefraversstatistikk() {
     }
 
     get("$SYKEFRAVERSSTATISTIKK_PATH/$FILTERVERDIER_PATH") {
-        call.respond(FilterverdierDto(GeografiService().hentFylkerOgKommuner()))
+        call.respond(FilterverdierDto(geografiService.hentFylkerOgKommuner()))
     }
 }
---
+++ src/main/kotlin/no/nav/lydia/virksomhet/VirksomhetService.kt
@@ -0,0 +1,14 @@
+package no.nav.lydia.virksomhet
+
+import no.nav.lydia.createDataSource
+import no.nav.lydia.sykefraversstatistikk.api.geografi.Fylke
+import no.nav.lydia.sykefraversstatistikk.api.geografi.Kommune
+
+class VirksomhetService {
+//    val virksomhetRepository = VirksomhetRepository()
+
+    fun hentVirksomheterFraFylkesnummer(fylkesnummmer: List<String>){
+
+
+    }
+}
