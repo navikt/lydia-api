ef7bf567e153033248ab10d5aaee627b2ae88429
Stefan A. Engelien <stefan.andreas.engelien@nav.no>
2022-02-09 09:26:19 +0100

    Fikset kafkakonsument -
    Var noe surr i containertestene med hvordan lydia-api konsumenten prøvde å koble seg til brokeren, men funker om brokeren ber om at man bruker port 9092. Må lese mer kafkadokumentasjon, gitt...

--- src/test/kotlin/no/nav/lydia/helper/KafkaContainerHelper.kt
+++ src/test/kotlin/no/nav/lydia/helper/KafkaContainerHelper.kt
@@ -1,6 +1,10 @@
 package no.nav.lydia.helper
 
+import no.nav.lydia.Kafka.Companion.statistikkTopic
 import org.apache.kafka.clients.CommonClientConfigs
+import org.apache.kafka.clients.admin.AdminClient
+import org.apache.kafka.clients.admin.AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG
+import org.apache.kafka.clients.admin.NewTopic
 import org.apache.kafka.clients.producer.KafkaProducer
 import org.apache.kafka.clients.producer.ProducerConfig
 import org.apache.kafka.common.config.SaslConfigs
@@ -12,27 +16,40 @@ import org.testcontainers.containers.Network
 import org.testcontainers.containers.output.Slf4jLogConsumer
 import org.testcontainers.containers.wait.strategy.HostPortWaitStrategy
 import org.testcontainers.utility.DockerImageName
-import java.util.*
+
 
 class KafkaContainerHelper(network: Network = Network.newNetwork(), log: Logger = LoggerFactory.getLogger(KafkaContainerHelper::class.java)) {
     private val kafkaNetworkAlias = "kafkaContainer"
     val kafkaContainer = KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:6.2.1"))
         .withNetwork(network)
-        .withLogConsumer(Slf4jLogConsumer(log).withPrefix(kafkaNetworkAlias).withSeparateOutputStreams())
         .withNetworkAliases(kafkaNetworkAlias)
+        .withExposedPorts(9092, 9093, KafkaContainer.ZOOKEEPER_PORT)
+        .withLogConsumer(Slf4jLogConsumer(log).withPrefix(kafkaNetworkAlias).withSeparateOutputStreams())
         .waitingFor(HostPortWaitStrategy())
         .apply {
-            start()
+            this.addEnv("KAFKA_ADVERTISED_LISTENERS", "BROKER://$kafkaNetworkAlias:9092,PLAINTEXT://$kafkaNetworkAlias:9092")
+            this.start()
+            this.createTopic(statistikkTopic)
         }
 
     fun envVars() = mapOf(
-        "KAFKA_BROKERS" to kafkaContainer.bootstrapServers
+        "KAFKA_BROKERS" to "BROKER://$kafkaNetworkAlias:9092,PLAINTEXT://$kafkaNetworkAlias:9092"
     )
 
-    fun producer(): KafkaProducer<String, String> =
+    private fun KafkaContainer.createTopic(vararg topics: String) {
+        val newTopics = topics
+            .map { topic -> NewTopic(topic, 1, 1.toShort()) }
+        AdminClient.create(mapOf(BOOTSTRAP_SERVERS_CONFIG to this.bootstrapServers)).use { admin ->
+            admin.createTopics(
+                newTopics
+            )
+        }
+    }
+
+    fun producer(bootstrapServers: String = kafkaContainer.bootstrapServers): KafkaProducer<String, String> =
         KafkaProducer(
         mapOf(
-            CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG to kafkaContainer.bootstrapServers,
+            CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG to bootstrapServers,
             CommonClientConfigs.SECURITY_PROTOCOL_CONFIG to "PLAINTEXT",
             ProducerConfig.ACKS_CONFIG to "all",
             ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION to "1",
