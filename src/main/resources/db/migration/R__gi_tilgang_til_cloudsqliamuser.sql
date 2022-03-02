e1847901c61edf9f995881cd96b80beb773f85fa
Ulrik Antoniussen Halmøy <ulrikhalmoy@gmail.com>
2022-03-02 11:33:36 +0100

    Gir tilgang til cloudsqliamuser som en repeatable migration
    Rollen blir også laget om den ikke skulle finnes fra før

---
+++ src/main/resources/db/migration/R__gi_tilgang_til_cloudsqliamuser.sql
@@ -0,0 +1,9 @@
+DO $$
+    BEGIN
+        CREATE ROLE cloudsqliamuser WITH NOLOGIN;
+    EXCEPTION WHEN DUPLICATE_OBJECT THEN
+        RAISE NOTICE 'not creating role cloudsqliamuser -- it already exists';
+    END
+$$;
+
+grant all on all tables in schema public to cloudsqliamuser;
