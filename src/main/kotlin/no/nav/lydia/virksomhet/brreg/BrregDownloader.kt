828ab89d44e3a72554499d9fe93e83e9af947b1e
Christian Klem <christian.klem@nav.no>
2022-03-08 15:48:10 +0100

    Tuner bedriftsimport for å kunne følge fremdrift bedre
     - også oppgradert gson til versjon 2.9.0

--- src/main/kotlin/no/nav/lydia/virksomhet/brreg/BrregDownloader.kt
+++ src/main/kotlin/no/nav/lydia/virksomhet/brreg/BrregDownloader.kt
@@ -9,11 +9,7 @@ import org.slf4j.LoggerFactory
 import java.io.InputStreamReader
 import java.nio.charset.StandardCharsets.UTF_8
 import java.util.zip.GZIPInputStream
-import kotlin.io.path.bufferedWriter
-import kotlin.io.path.createTempFile
-import kotlin.io.path.fileSize
-import kotlin.io.path.inputStream
-import kotlin.io.path.pathString
+import kotlin.io.path.*
 
 // Ref: https://data.brreg.no/enhetsregisteret/api/docs/index.html#enheter-lastned
 class BrregDownloader(
@@ -31,7 +27,7 @@ class BrregDownloader(
             url.httpGet()
         request[Headers.ACCEPT] = underEnhetApplicationType
         val (_, _, result) = request.response()
-        result.fold(success = {
+        val temporærFil = result.fold(success = {
             val file = createTempFile(prefix = "${System.currentTimeMillis()}", suffix = "brreg-nedlasting.json");
             GZIPInputStream(it.inputStream())
                 .bufferedReader(UTF_8)
@@ -43,6 +39,17 @@ class BrregDownloader(
                     }
                 }
             log.info("Lastet ned decompressed fil med størrelse ${file.fileSize()} og path ${file.pathString}")
+            file
+        }, failure = {
+            log.error("Kall mot BRREG feilet: $it")
+            null
+        })
+
+        temporærFil?.let { file ->
+            var importerteBedrifter = 0
+            var feilendeBedrifter = 0
+            var ikkeRelevanteBedrifter = 0
+            log.info("Starter å importere bedrifter fra temporær fil")
             val gson = GsonBuilder().serializeNulls().create()
             JsonReader(InputStreamReader(file.inputStream())).use { reader ->
                 reader.beginArray()
@@ -51,26 +58,34 @@ class BrregDownloader(
                     when (virksomhet) {
                         null -> {
                             log.debug("Skipper lagring av virksomhet da den er null fra JsonReader")
+                            feilendeBedrifter++
                         }
                         else -> {
                             try {
                                 virksomhet.beliggenhetsadresse?.let { adresse ->
-                                    if (adresse.erRelevant())
+                                    if (adresse.erRelevant()) {
                                         virksomhetRepository.insert(virksomhet = virksomhet)
+                                        importerteBedrifter++
+                                    } else {
+                                        ikkeRelevanteBedrifter++
+                                    }
                                 }
                             } catch (e: Exception) {
+                                feilendeBedrifter++
                                 log.error("Lagring av virksomhet feilet", e)
                             }
                         }
                     }
 
+                    if ((importerteBedrifter + feilendeBedrifter + ikkeRelevanteBedrifter) % 1000 == 0) {
+                        log.info("Bedriftsimport fremdrift: Importerte bedrifter: $importerteBedrifter, Feilende bedrifter: $feilendeBedrifter, Ikke relevante bedrifter: $ikkeRelevanteBedrifter")
+                    }
                 }
                 reader.endArray()
             }
-            log.info("Ferdig å importere virksomheter")
-        }, failure = {
-            log.error("Kall mot BRREG feilet: $it")
-        })
 
+            log.info("Bedriftsimport ferig! Importerte bedrifter: $importerteBedrifter, Feilende bedrifter: $feilendeBedrifter, Ikke relevante bedrifter: $ikkeRelevanteBedrifter")
+            file.deleteIfExists()
+        }
     }
 }
