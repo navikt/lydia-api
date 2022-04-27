5c9c5fae8ef6d535bfac88dec3317148d20a3ccc
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2022-04-27 11:10:16 +0200

    Bytt ut contains med in-operator

--- src/main/kotlin/no/nav/lydia/sykefraversstatistikk/api/geografi/GeografiService.kt
+++ src/main/kotlin/no/nav/lydia/sykefraversstatistikk/api/geografi/GeografiService.kt
@@ -92,8 +92,8 @@ class GeografiService {
             .toMutableList()
         return fylkerMappetMedKommuner
             .apply {
-                add(FylkeOgKommuner(Fylke("Øst-Viken", "30"), alleKommuner.filter { ØST_VIKEN_KOMMUNENR.contains(it.nummer) }))
-                add(FylkeOgKommuner(Fylke("Vest-Viken", "30"), alleKommuner.filter { VEST_VIKEN_KOMMUNENR.contains(it.nummer) }))
+                add(FylkeOgKommuner(Fylke("Øst-Viken", "30"), alleKommuner.filter { it.nummer in ØST_VIKEN_KOMMUNENR }))
+                add(FylkeOgKommuner(Fylke("Vest-Viken", "30"), alleKommuner.filter { it.nummer in VEST_VIKEN_KOMMUNENR }))
             }
     }
 
