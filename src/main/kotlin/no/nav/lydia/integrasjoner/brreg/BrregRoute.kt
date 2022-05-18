19fb5600c13ff7e4b4fb6a69d35de21e186025f7
Christian Klem <christian.klem@nav.no>
2022-05-18 15:34:15 +0200

    Virksomhetsimport kjører nå i en co-routine
    Co-authored-by: Ulrik Antoniussen Halmøy <ulrik.antoniussen.halmoy@nav.no>
    Co-authored-by: Tanet Trimas <tanet.trimas@nav.no>

--- src/main/kotlin/no/nav/lydia/integrasjoner/brreg/BrregRoute.kt
+++ src/main/kotlin/no/nav/lydia/integrasjoner/brreg/BrregRoute.kt
@@ -4,6 +4,7 @@ import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.coroutines.launch
 
 val VIRKSOMHETSIMPORT_PATH = "internal/virksomhetsimport"
 
@@ -13,7 +14,9 @@ fun Route.virksomhetsImport(brregDownloader: BrregDownloader) {
             call.application.log.warn("Kjører allerede import av bedrifter.")
             return@get call.respond(HttpStatusCode.Conflict)
         }
-        brregDownloader.lastNed()
+        launch {
+            brregDownloader.lastNed()
+        }
         call.respond(HttpStatusCode.OK)
     }
 }
