a6e0f1b647a1cdd22d5c875c28af04fc1ea7cc57
Ulrik Antoniussen Halmøy <ulrik.antoniussen.halmoy@nav.no>
2022-05-18 13:02:31 +0200

    Refaktorerer import av underenheter fra Brreg
    Co-authored-by: Christian Klem <christian.klem@nav.no>
    Co-authored-by: Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>

--- src/main/kotlin/no/nav/lydia/integrasjoner/brreg/BrregDownloader.kt
+++ src/main/kotlin/no/nav/lydia/integrasjoner/brreg/BrregDownloader.kt
@@ -5,9 +5,12 @@ import com.github.kittinunf.fuel.httpGet
 import com.google.gson.GsonBuilder
 import com.google.gson.stream.JsonReader
 import no.nav.lydia.virksomhet.VirksomhetRepository
+import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import java.io.IOException
 import java.io.InputStreamReader
 import java.nio.charset.StandardCharsets.UTF_8
+import java.nio.file.Path
 import java.util.zip.GZIPInputStream
 import kotlin.io.path.*
 
@@ -20,72 +23,94 @@ class BrregDownloader(
         const val underEnhetApplicationType = "application/vnd.brreg.enhetsregisteret.underenhet.v1+gzip;charset=UTF-8"
     }
 
-    val log = LoggerFactory.getLogger(this.javaClass)
+    val log: Logger = LoggerFactory.getLogger(this.javaClass)
 
     fun lastNed() {
-        val request =
-            url.httpGet()
+        val brregKomprimert = lagreTilFil(lastNedUnderEnheterSomZip(), "brreg-nedlasting.json.gz")
+        val brregUkomprimert = unzipFil(brregKomprimert)
+        brregKomprimert.deleteIfExists()
+        importerVirksomheterFraFil(brregUkomprimert)
+        brregUkomprimert.deleteIfExists()
+    }
+
+    private fun lastNedUnderEnheterSomZip(): ByteArray {
+        val request = url.httpGet()
         request[Headers.ACCEPT] = underEnhetApplicationType
         val (_, _, result) = request.response()
-        val temporærFil = result.fold(success = {
-            val file = createTempFile(prefix = "${System.currentTimeMillis()}", suffix = "brreg-nedlasting.json")
-            GZIPInputStream(it.inputStream())
-                .bufferedReader(UTF_8)
-                .useLines { lines ->
-                    file.bufferedWriter().use { bw ->
-                        lines.forEach {
-                            bw.write(it)
-                        }
-                    }
-                }
-            log.info("Lastet ned decompressed fil med størrelse ${file.fileSize()} og path ${file.pathString}")
-            file
+        return result.fold(success = { bytes ->
+            log.info("Lastet ned komprimert fil med størrelse ${bytes.size / 1024 / 1024} MB")
+            bytes
         }, failure = {
-            log.error("Kall mot BRREG feilet: $it")
-            null
+            val feilmelding = "Kall mot BRREG feilet"
+            log.error("$feilmelding: $it")
+            throw IOException(feilmelding)
         })
+    }
 
-        temporærFil?.let { file ->
-            var importerteBedrifter = 0
-            var feilendeBedrifter = 0
-            var ikkeRelevanteBedrifter = 0
-            log.info("Starter å importere bedrifter fra temporær fil")
-            val gson = GsonBuilder().serializeNulls().create()
-            JsonReader(InputStreamReader(file.inputStream())).use { reader ->
-                reader.beginArray()
-                while (reader.hasNext()) {
-                    val virksomhet = gson.fromJson<BrregVirksomhetDto>(reader, BrregVirksomhetDto::class.java)
-                    when (virksomhet) {
-                        null -> {
-                            log.debug("Skipper lagring av virksomhet da den er null fra JsonReader")
-                            feilendeBedrifter++
-                        }
-                        else -> {
-                            try {
-                                virksomhet.beliggenhetsadresse?.let { adresse ->
-                                    if (adresse.erRelevant()) {
-                                        virksomhetRepository.insert(virksomhet = virksomhet)
-                                        importerteBedrifter++
-                                    } else {
-                                        ikkeRelevanteBedrifter++
-                                    }
-                                }
-                            } catch (e: Exception) {
-                                feilendeBedrifter++
-                                log.error("Lagring av virksomhet feilet", e)
-                            }
-                        }
+    private fun importerVirksomheterFraFil(brregUkomprimert: Path) {
+        var importerteBedrifter = 0
+        var feilendeBedrifter = 0
+        var ikkeRelevanteBedrifter = 0
+        log.info("Starter å importere bedrifter fra temporær fil")
+        val gson = GsonBuilder().serializeNulls().create()
+        JsonReader(InputStreamReader(brregUkomprimert.inputStream())).use { reader ->
+            reader.beginArray()
+            while (reader.hasNext()) {
+                val virksomhet = gson.fromJson<BrregVirksomhetDto>(reader, BrregVirksomhetDto::class.java)
+                when (virksomhet) {
+                    null -> {
+                        log.debug("Skipper lagring av virksomhet da den er null fra JsonReader")
+                        feilendeBedrifter++
                     }
-
-                    if ((importerteBedrifter + feilendeBedrifter + ikkeRelevanteBedrifter) % 1000 == 0) {
-                        log.info("Bedriftsimport fremdrift: Importerte bedrifter: $importerteBedrifter, Feilende bedrifter: $feilendeBedrifter, Ikke relevante bedrifter: $ikkeRelevanteBedrifter")
+                    else -> {
+                        try {
+                            virksomhet.beliggenhetsadresse?.let { adresse ->
+                                if (adresse.erRelevant()) {
+                                    virksomhetRepository.insert(virksomhet = virksomhet)
+                                    importerteBedrifter++
+                                } else {
+                                    ikkeRelevanteBedrifter++
+                                }
+                            }
+                        } catch (e: Exception) {
+                            feilendeBedrifter++
+                            log.error("Lagring av virksomhet feilet", e)
+                        }
                     }
                 }
-                reader.endArray()
-            }
 
-            log.info("Bedriftsimport ferig! Importerte bedrifter: $importerteBedrifter, Feilende bedrifter: $feilendeBedrifter, Ikke relevante bedrifter: $ikkeRelevanteBedrifter")
-            file.deleteIfExists()
+                if ((importerteBedrifter + feilendeBedrifter + ikkeRelevanteBedrifter) % 1000 == 0) {
+                    log.info("Bedriftsimport fremdrift: Importerte bedrifter: $importerteBedrifter, Feilende bedrifter: $feilendeBedrifter, Ikke relevante bedrifter: $ikkeRelevanteBedrifter")
+                }
+            }
+            reader.endArray()
         }
+        log.info("Bedriftsimport ferdig! Importerte bedrifter: $importerteBedrifter, Feilende bedrifter: $feilendeBedrifter, Ikke relevante bedrifter: $ikkeRelevanteBedrifter")
     }
+
+    private fun unzipFil(komprimertFil: Path): Path {
+        log.info("Unzipper ${komprimertFil.fileName} med størrelse ${komprimertFil.sizeInMb()} MB")
+        val ukomprimertFil = createTempFile(prefix = "${System.currentTimeMillis()}", suffix = "brreg-nedlasting.json")
+        GZIPInputStream(komprimertFil.inputStream())
+            .bufferedReader(UTF_8)
+            .useLines { lines ->
+                ukomprimertFil.bufferedWriter().use { bw ->
+                    lines.forEach {
+                        bw.write(it)
+                    }
+                }
+            }
+        log.info("Unzippet ${ukomprimertFil.fileName} til størrelse ${ukomprimertFil.sizeInMb()} MB")
+        return ukomprimertFil
+    }
+
+    private fun lagreTilFil(filinnhold: ByteArray, filnavn: String) =
+        createTempFile(
+            prefix = "${System.currentTimeMillis()}",
+            suffix = filnavn
+        ).also { file -> file.writeBytes(filinnhold) }
+
 }
+
+
+fun Path.sizeInMb() = this.fileSize() / 1024 / 1024
