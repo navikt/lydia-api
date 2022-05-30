34c2bd9bbb0b680aa2bf5f2ca2eaa7ac1fe7c4e4
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2022-05-30 13:51:30 +0200

    Kafka produsent lages ved opprettelse av appen

--- src/main/kotlin/no/nav/lydia/NaisEnvironment.kt
+++ src/main/kotlin/no/nav/lydia/NaisEnvironment.kt
@@ -2,8 +2,10 @@ package no.nav.lydia
 
 import org.apache.kafka.clients.CommonClientConfigs
 import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.clients.producer.ProducerConfig
 import org.apache.kafka.common.config.SaslConfigs
 import org.apache.kafka.common.config.SslConfigs
+import org.apache.kafka.common.serialization.StringSerializer
 import java.net.URL
 
 class NaisEnvironment(
@@ -71,6 +73,34 @@ class Kafka(
         const val clientId: String = "lydia-api"
     }
 
+    fun producerProperties(): Map<String, Any> {
+        val producerConfigs = mutableMapOf(
+            ProducerConfig.BOOTSTRAP_SERVERS_CONFIG to brokers,
+            ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG to StringSerializer::class.java,
+            ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG to StringSerializer::class.java,
+            ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG to true, // Den sikrer rekkefølge
+            ProducerConfig.ACKS_CONFIG to "all", // Den sikrer at data ikke mistes
+            ProducerConfig.CLIENT_ID_CONFIG to clientId
+        )
+        if (truststoreLocation.isNotEmpty()) {
+            producerConfigs + securityConfigs()
+        }
+        return producerConfigs.toMap()
+    }
+
+    fun securityConfigs() =
+        mapOf(
+            CommonClientConfigs.SECURITY_PROTOCOL_CONFIG to "SSL",
+            SslConfigs.SSL_ENDPOINT_IDENTIFICATION_ALGORITHM_CONFIG to "",
+            SslConfigs.SSL_TRUSTSTORE_TYPE_CONFIG to "JKS",
+            SslConfigs.SSL_KEYSTORE_TYPE_CONFIG to "PKCS12",
+            SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG to truststoreLocation,
+            SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG to credstorePassword,
+            SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG to keystoreLocation,
+            SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG to credstorePassword,
+            SslConfigs.SSL_KEY_PASSWORD_CONFIG to credstorePassword
+        )
+
     fun consumerProperties() =
         baseConsumerProperties().apply {
             // TODO: Finn smidigere måte å få tester til å kjøre
@@ -78,15 +108,7 @@ class Kafka(
                 put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "PLAINTEXT")
                 put(SaslConfigs.SASL_MECHANISM, "PLAIN")
             } else {
-                put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SSL")
-                put(SslConfigs.SSL_ENDPOINT_IDENTIFICATION_ALGORITHM_CONFIG, "")
-                put(SslConfigs.SSL_TRUSTSTORE_TYPE_CONFIG, "JKS")
-                put(SslConfigs.SSL_KEYSTORE_TYPE_CONFIG, "PKCS12")
-                put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, truststoreLocation)
-                put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, credstorePassword)
-                put(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG, keystoreLocation)
-                put(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG, credstorePassword)
-                put(SslConfigs.SSL_KEY_PASSWORD_CONFIG, credstorePassword)
+                putAll(securityConfigs())
             }
         }
 
