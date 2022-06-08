9badebfd3694ff8fa68113da5a7d1fcd0b82c1f2
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2022-06-08 14:56:56 +0200

    Skriv transformers som gjør om en iasakshendelse til en json som sendes på kafka

--- src/main/kotlin/no/nav/lydia/ia/eksport/IaProdusent.kt
+++ src/main/kotlin/no/nav/lydia/ia/eksport/IaProdusent.kt
@@ -3,14 +3,16 @@ package no.nav.lydia.ia.eksport
 import no.nav.lydia.Observer
 import no.nav.lydia.ia.sak.domene.IASakshendelse
 import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.ProducerRecord
 
-class IaProdusent(private val producer: KafkaProducer<String, String>) : Observer<IASakshendelse> {
+class IaProdusent(private val producer: KafkaProducer<String, String>, private val topic: String) : Observer<IASakshendelse> {
     internal fun stop() {
         producer.close()
     }
 
     override fun receive(input: IASakshendelse) {
-        print("Sendte iasak $input")
-        //producer.send()
+        val kafkaMelding = input.tilKafkaMelding()
+        val melding = ProducerRecord(topic, kafkaMelding.first, kafkaMelding.second)
+        producer.send(melding)
     }
 }
