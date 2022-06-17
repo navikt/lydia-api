578936476bb9d1e48d098dabf607eeca31477321
Christian Klem <christian.klem@nav.no>
2022-06-17 11:33:20 +0200

    Returner valgte begrunnelser i samarbeidshistorikken
    Co-authored-by: Ulrik Antoniussen Halmøy <ulrik.antoniussen.halmoy@nav.no>
    Co-authored-by: Tanet Trimas <tanet.trimas@nav.no>

--- src/main/kotlin/no/nav/lydia/ia/sak/db/IASakshendelseRepository.kt
+++ src/main/kotlin/no/nav/lydia/ia/sak/db/IASakshendelseRepository.kt
@@ -6,6 +6,10 @@ import kotliquery.sessionOf
 import kotliquery.using
 import no.nav.lydia.ia.sak.domene.IASakshendelse
 import no.nav.lydia.ia.sak.domene.SaksHendelsestype
+import no.nav.lydia.ia.sak.domene.VirksomhetIkkeAktuellHendelse
+import no.nav.lydia.ia.årsak.domene.BegrunnelseType
+import no.nav.lydia.ia.årsak.domene.ValgtÅrsak
+import no.nav.lydia.ia.årsak.domene.ÅrsakType
 import no.nav.lydia.sykefraversstatistikk.api.geografi.NavEnheter
 import javax.sql.DataSource
 
@@ -14,23 +18,43 @@ class IASakshendelseRepository(val dataSource: DataSource) {
     fun hentHendelser(saksnummer: String) =
         using(sessionOf(dataSource)) { session ->
             session.run(
-                queryOf("""
-                    SELECT * FROM ia_sak_hendelse
+                queryOf(
+                    """
+                    SELECT 
+                        id,
+                        type,
+                        orgnr,
+                        opprettet_av,
+                        saksnummer,
+                        opprettet,
+                        aarsak_enum,
+                        array_agg(begrunnelse_enum) as begrunnelser
+                    FROM ia_sak_hendelse
+                    LEFT JOIN hendelse_begrunnelse ON (ia_sak_hendelse.id = hendelse_begrunnelse.hendelse_id) 
                     WHERE saksnummer = :saksnummer
-                    AND orgnr NOT in ${NavEnheter.enheterSomSkalSkjermes.joinToString(prefix = "(", postfix = ")", separator = ",") { s -> "\'$s\'"}}
+                    AND orgnr NOT in ${
+                        NavEnheter.enheterSomSkalSkjermes.joinToString(
+                            prefix = "(",
+                            postfix = ")",
+                            separator = ","
+                        ) { s -> "\'$s\'" }
+                    }
+                    GROUP BY aarsak_enum, id, type, orgnr, opprettet_av, saksnummer, opprettet
                     ORDER BY id ASC
-                """.trimIndent(),
-                mapOf(
-                    "saksnummer" to saksnummer
-                ))
-                .map(this::mapRow).asList
+                    """.trimIndent(),
+                        mapOf(
+                            "saksnummer" to saksnummer
+                        )
+                    )
+                    .map(this::mapRow).asList
             )
         }
 
-    fun lagreHendelse(hendelse : IASakshendelse) =
+    fun lagreHendelse(hendelse: IASakshendelse) =
         using(sessionOf(dataSource)) { session ->
             session.run(
-                queryOf("""
+                queryOf(
+                    """
                     INSERT INTO ia_sak_hendelse (
                         id,
                         saksnummer,
@@ -62,13 +86,29 @@ class IASakshendelseRepository(val dataSource: DataSource) {
         }
 
     private fun mapRow(row: Row): IASakshendelse {
-        return IASakshendelse(
+        val valgtÅrsak = årsakFraDatabase(row.stringOrNull("aarsak_enum"), row.array("begrunnelser"))
+            ?: return IASakshendelse(
+                id = row.string("id"),
+                hendelsesType = SaksHendelsestype.valueOf(row.string("type")),
+                orgnummer = row.string("orgnr"),
+                opprettetAv = row.string("opprettet_av"),
+                saksnummer = row.string("saksnummer"),
+                opprettetTidspunkt = row.localDateTime("opprettet"),
+            )
+        return VirksomhetIkkeAktuellHendelse(
             id = row.string("id"),
-            hendelsesType = SaksHendelsestype.valueOf(row.string("type")),
             orgnummer = row.string("orgnr"),
             opprettetAv = row.string("opprettet_av"),
             saksnummer = row.string("saksnummer"),
             opprettetTidspunkt = row.localDateTime("opprettet"),
+            valgtÅrsak = valgtÅrsak
         )
     }
+
+    private fun årsakFraDatabase(årsak: String?, begrunnelser: Array<String?>) =
+        årsak?.let {
+            val valgtÅrsak = ÅrsakType.valueOf(it)
+            val valgtBegrunnelser = begrunnelser.filterNotNull().map(BegrunnelseType::valueOf)
+            ValgtÅrsak(type = valgtÅrsak, begrunnelser = valgtBegrunnelser)
+        }
 }
