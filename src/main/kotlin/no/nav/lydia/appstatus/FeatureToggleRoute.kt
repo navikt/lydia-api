ef6bf1eb48724c152db1883a29067e9924cc37fa
Ulrik Antoniussen Halmøy <ulrik.antoniussen.halmoy@nav.no>
2022-06-24 13:16:07 +0200

    Kaizen: Navngir featuretoggles
    Co-authored-by: Tanet Trimas <tanet.trimas@nav.no>
    Co-authored-by: Christian Klem <christian.klem@nav.no>

--- src/main/kotlin/no/nav/lydia/appstatus/FeatureToggleRoute.kt
+++ src/main/kotlin/no/nav/lydia/appstatus/FeatureToggleRoute.kt
@@ -7,10 +7,17 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 const val FEATURE_TOGGLE_PATH = "unleash"
+const val FEATURE_TOGGLE_TEST_PATH = "$FEATURE_TOGGLE_PATH/test"
+
+object UnleashToggles {
+    const val testToggle = "pia.tester-unleash"
+    const val nyeStatuserToggle = "pia.nye-statuser"
+}
+
 
 fun Route.featureToggle(unleash: Unleash) {
-    get(FEATURE_TOGGLE_PATH) {
-        if (unleash.isEnabled("pia.tester-unleash")){
+    get(FEATURE_TOGGLE_TEST_PATH) {
+        if (unleash.isEnabled(UnleashToggles.testToggle)){
             call.respond(status = HttpStatusCode.OK, message = "Enabled!")
         } else{
             call.respond(status = HttpStatusCode.NotImplemented, message = "Not implemented!")
