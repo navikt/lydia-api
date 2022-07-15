560010e8ad2bfe384249bd5ebb1b6878a51f1602
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2022-07-15 15:42:06 +0200

    Opprydning av ubrukte klasser
    Co-authored-by: Stefan A. Engelien <stefan.andreas.engelien@nav.no>

--- src/main/kotlin/no/nav/lydia/ia/sak/api/IASakshendelseDto.kt
+++ src/main/kotlin/no/nav/lydia/ia/sak/api/IASakshendelseDto.kt
@@ -1,10 +1,9 @@
 package no.nav.lydia.ia.sak.api
 
 
+import kotlinx.datetime.LocalDateTime
 import kotlinx.serialization.Serializable
-import no.nav.lydia.ia.sak.domene.IASakshendelse
 import no.nav.lydia.ia.sak.domene.SaksHendelsestype
-import java.time.LocalDateTime
 
 @Serializable
 open class IASakshendelseDto(
@@ -22,20 +21,6 @@ class IASakshendelseOppsummeringDto(
     val saksnummer: String,
     val hendelsestype: SaksHendelsestype,
     val opprettetAv: String,
-    @Serializable(with = LocalDateTimeSerializer::class)
     val opprettetTidspunkt: LocalDateTime,
-    ) {
-    companion object {
-        fun List<IASakshendelse>.toDto() = this.map { it.toDto() }
-
-        fun IASakshendelse.toDto() = IASakshendelseOppsummeringDto(
-            id = this.id,
-            orgnummer = this.orgnummer,
-            saksnummer = this.saksnummer,
-            hendelsestype = this.hendelsesType,
-            opprettetAv = this.opprettetAv,
-            opprettetTidspunkt = this.opprettetTidspunkt,
-        )
-    }
-}
+)
 
