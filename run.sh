1c7a843272fae0aa3f5d786b25d1a1213cc18c99
Thomas Dufourd <thomas.dufourd@gmail.com>
2022-12-05 13:41:34 +0100

    Fix: legge til env variabel for topic til sykefraværsstatistikk til virksomhet i lokal oppsett (docker-compose). Unngå unødvendig log (som f.eks sql inserts) i output til run.sh scriptet.
    Co-authored-by: Christian Klem <christian.klem@nav.no>
    Co-authored-by: Ingrid Fosså <ingrid.fossa@nav.no>

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@
 docker-compose up -d
 curl -o db_script.sql https://raw.githubusercontent.com/navikt/lydia-api/main/scripts/db/lydia_api_container_db_localhost-2022_12_05_10_57_42-dump.sql
 sleep 3
-PGPASSWORD=test psql -h localhost -p 5432 -U postgres -f db_script.sql
+PGPASSWORD=test psql -h localhost -p 5432 -U postgres -f db_script.sql > /dev/null
 rm db_script.sql
