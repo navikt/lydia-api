f1a89c441cde7aef7e43d8499dce581ad4ea0f02
Christian Klem <christian.klem@nav.no>
2023-01-18 14:41:34 +0100

    Refaktorert uthenting av sykefraværsstatistikk til søk slik at det blir mer lettlest
    Co-authored-by: Per-Christian Nielsen <per-christian.nielsen@nav.no>
    Co-authored-by: Ingrid Fosså <ingrid.fossa@nav.no>
    Co-authored-by: Nima Jimale <nima.jimale@nav.no>

--- src/main/kotlin/no/nav/lydia/sykefraversstatistikk/SykefraværsstatistikkSiste4KvartalRepository.kt
+++ src/main/kotlin/no/nav/lydia/sykefraversstatistikk/SykefraværsstatistikkSiste4KvartalRepository.kt
@@ -23,85 +23,146 @@ import javax.sql.DataSource
 
 class SykefraværsstatistikkSiste4KvartalRepository(val dataSource: DataSource) {
     private val gson: Gson = GsonBuilder().create()
+
     fun hentSykefravær(
         søkeparametere: Søkeparametere,
     ) = using(sessionOf(dataSource)) { session ->
         val næringsgrupperMedBransjer = søkeparametere.næringsgrupperMedBransjer()
         val sektorer = søkeparametere.sektor.map { it.kode }.toSet()
-        val tmpKommuneTabell = "kommuner"
-        val tmpNæringTabell = "naringer"
-        val tmpNavIdenterTabell = "nav_identer"
-        val tmpSektorTabell = "sektorer"
+
         val sql = """
-                    WITH 
-                        ${filterVerdi(tmpKommuneTabell, søkeparametere.kommunenummer)},
-                        ${filterVerdi(tmpNæringTabell, næringsgrupperMedBransjer)},
-                        ${filterVerdi(tmpNavIdenterTabell, søkeparametere.navIdenter)},
-                        ${filterVerdi(tmpSektorTabell, sektorer)}
-                    SELECT
-                        virksomhet.orgnr,
-                        virksomhet.navn,
-                        virksomhet.kommune,
-                        virksomhet.kommunenummer,
-                        statistikk.arstall,
-                        statistikk.kvartal,
-                        statistikk.antall_personer,
-                        statistikk_siste4.tapte_dagsverk,
-                        statistikk_siste4.mulige_dagsverk,
-                        statistikk_siste4.prosent,
-                        statistikk_siste4.maskert,
-                        statistikk_siste4.sist_endret,
-                        ia_sak.status,
-                        ia_sak.eid_av,
-                        ia_sak.endret
-                    ${
-            filter(
-                tmpKommuneTabell = tmpKommuneTabell,
-                tmpNavIdenterTabell = tmpNavIdenterTabell,
-                tmpNæringTabell = tmpNæringTabell,
-                tmpSektorTabell = tmpSektorTabell,
-                søkeparametere = søkeparametere
-            )
-        }
-                    GROUP BY 
-                        virksomhet.orgnr,
-                        virksomhet.navn,
-                        virksomhet.kommune,
-                        virksomhet.kommunenummer,
-                        statistikk.arstall,
-                        statistikk.kvartal,
-                        statistikk.antall_personer,
-                        statistikk_siste4.tapte_dagsverk,
-                        statistikk_siste4.mulige_dagsverk,
-                        statistikk_siste4.prosent,
-                        statistikk_siste4.maskert,
-                        statistikk_siste4.sist_endret,
-                        ia_sak.status,
-                        ia_sak.eid_av,
-                        ia_sak.endret
-                    ${søkeparametere.sorteringsnøkkel.tilOrderBy()} ${søkeparametere.sorteringsretning} NULLS LAST
-                    LIMIT ${søkeparametere.virksomheterPerSide()}
-                    OFFSET ${søkeparametere.offset()}
-                """.trimIndent()
+            SELECT
+                virksomhet.orgnr,
+                virksomhet.navn,
+                virksomhet.kommune,
+                virksomhet.kommunenummer,
+                statistikk.arstall,
+                statistikk.kvartal,
+                statistikk.antall_personer,
+                statistikk_siste4.tapte_dagsverk,
+                statistikk_siste4.mulige_dagsverk,
+                statistikk_siste4.prosent,
+                statistikk_siste4.maskert,
+                statistikk_siste4.sist_endret,
+                ia_sak.status,
+                ia_sak.eid_av,
+                ia_sak.endret
+            FROM 
+                sykefravar_statistikk_virksomhet AS statistikk
+                JOIN virksomhet USING (orgnr)
+                JOIN sykefravar_statistikk_virksomhet_siste_4_kvartal AS statistikk_siste4 USING (orgnr)
+                ${
+                    if (sektorer.isNotEmpty()) " LEFT JOIN virksomhet_statistikk_metadata USING (orgnr) "
+                    else ""
+                }
+                LEFT JOIN ia_sak ON (
+                    (ia_sak.orgnr = statistikk.orgnr) AND
+                    ia_sak.endret = (select max(endret) from ia_sak iasak2 where iasak2.orgnr = statistikk.orgnr)
+                )
+                ${
+                    if (næringsgrupperMedBransjer.isNotEmpty()) " JOIN virksomhet_naring AS vn on (virksomhet.id = vn.virksomhet) "
+                    else ""
+                }
+                
+            WHERE 
+                statistikk.arstall = :arstall
+                AND statistikk.kvartal = :kvartal
+                
+                ${filtrerPåBransjeOgNæring(søkeparametere = søkeparametere)}
+                ${filtrerPåKommuner(søkeparametere = søkeparametere)}
+                ${filtrerPåStatus(søkeparametere = søkeparametere)}
+                ${filtrerPåSektor(søkeparametere = søkeparametere)}
+                ${filtrerPåEiere(søkeparametere = søkeparametere)}
+                
+                ${søkeparametere.sykefraværsprosentFra?.let { " AND statistikk_siste4.prosent >= $it " } ?: ""}
+                ${søkeparametere.sykefraværsprosentTil?.let { " AND statistikk_siste4.prosent <= $it " } ?: ""}
+                
+                ${søkeparametere.ansatteFra?.let { " AND statistikk.antall_personer >= $it " } ?: ""}
+                ${søkeparametere.ansatteTil?.let { " AND statistikk.antall_personer <= $it " } ?: ""}
+                
+                AND virksomhet.status = '${VirksomhetStatus.AKTIV.name}'
+            GROUP BY 
+                virksomhet.orgnr,
+                virksomhet.navn,
+                virksomhet.kommune,
+                virksomhet.kommunenummer,
+                statistikk.arstall,
+                statistikk.kvartal,
+                statistikk.antall_personer,
+                statistikk_siste4.tapte_dagsverk,
+                statistikk_siste4.mulige_dagsverk,
+                statistikk_siste4.prosent,
+                statistikk_siste4.maskert,
+                statistikk_siste4.sist_endret,
+                ia_sak.status,
+                ia_sak.eid_av,
+                ia_sak.endret
+            ${søkeparametere.sorteringsnøkkel.tilOrderBy()} ${søkeparametere.sorteringsretning} NULLS LAST
+            LIMIT ${søkeparametere.virksomheterPerSide()}
+            OFFSET ${søkeparametere.offset()}
+        """.trimIndent()
 
         val query = queryOf(
-            statement = sql, mapOf(
-                tmpKommuneTabell to session.connection.underlying.createArrayOf(
-                    "text", søkeparametere.kommunenummer.toTypedArray()
-                ),
-                tmpNæringTabell to session.connection.underlying.createArrayOf(
-                    "text", næringsgrupperMedBransjer.toTypedArray()
-                ),
-                tmpNavIdenterTabell to session.connection.underlying.createArrayOf(
-                    "text", søkeparametere.navIdenter.toTypedArray()
-                ),
-                tmpSektorTabell to session.connection.underlying.createArrayOf(
-                    "text", sektorer.toTypedArray()
-                ),
-                "kvartal" to søkeparametere.periode.kvartal, "arstall" to søkeparametere.periode.årstall
+            statement = sql,
+            mapOf(
+                "kvartal" to søkeparametere.periode.kvartal,
+                "arstall" to søkeparametere.periode.årstall,
+
+                "naringer" to session.createArrayOf("text", næringsgrupperMedBransjer),
+                "kommuner" to session.createArrayOf("text", søkeparametere.kommunenummer),
+                "sektorer" to session.createArrayOf("text", sektorer),
+                "eiere" to session.createArrayOf("text", søkeparametere.navIdenter),
             )
         ).map(this::mapRow).asList
         session.run(query)
+    }
+
+    private fun filtrerPåEiere(søkeparametere: Søkeparametere) =
+        if (søkeparametere.navIdenter.isEmpty()) ""
+        else " AND ia_sak.eid_av in (select unnest(:eiere)) "
+
+    private fun filtrerPåSektor(søkeparametere: Søkeparametere) =
+        if(søkeparametere.sektor.isEmpty()) ""
+        else " AND virksomhet_statistikk_metadata.sektor in (select unnest(:sektorer)) "
+
+    private fun filtrerPåStatus(søkeparametere: Søkeparametere) =
+        søkeparametere.status?.let { status ->
+            when (status) {
+                IAProsessStatus.IKKE_AKTIV -> " AND (ia_sak.status IS NULL " +
+                        "OR ((ia_sak.status = 'IKKE_AKTUELL' OR ia_sak.status = 'FULLFØRT' OR ia_sak.status = 'SLETTET') " +
+                        "AND ia_sak.endret < '${LocalDate.now().minusDays(ANTALL_DAGER_FØR_SAK_LÅSES)}'))"
+
+                IAProsessStatus.IKKE_AKTUELL, IAProsessStatus.FULLFØRT, IAProsessStatus.SLETTET ->
+                    " AND ia_sak.status = '$status' " +
+                            "AND ia_sak.endret >= '${LocalDate.now().minusDays(ANTALL_DAGER_FØR_SAK_LÅSES)}'"
+
+                else -> " AND ia_sak.status = '$status'"
+            }
+        } ?: ""
+
+    private fun filtrerPåKommuner(søkeparametere: Søkeparametere) =
+        if (søkeparametere.kommunenummer.isEmpty()) ""
+        else " AND virksomhet.kommunenummer in (select unnest(:kommuner)) "
+
+    private fun filtrerPåBransjeOgNæring(søkeparametere: Søkeparametere): String {
+        val næringsgrupperMedBransjer = søkeparametere.næringsgrupperMedBransjer()
+        return if (næringsgrupperMedBransjer.isEmpty())
+            ""
+        else
+            """
+                AND (
+                    substr(vn.narings_kode, 1, 2) in (select unnest(:naringer))
+                    ${
+                        if (søkeparametere.bransjeprogram.isNotEmpty()) {
+                            val koder = søkeparametere.bransjeprogram.flatMap { it.næringskoder }.groupBy {
+                                it.length
+                            }
+                            val femsifrede = koder[5]?.joinToString { "'${it.take(2)}.${it.takeLast(3)}'" }
+                            femsifrede?.let { "OR (vn.narings_kode in (select (unnest(:naringer))))" } ?: ""
+                        } else ""
+                    }
+                    )
+            """.trimIndent()
     }
 
     private fun Sorteringsnøkkel.tilOrderBy(): String {
@@ -117,118 +178,60 @@ class SykefraværsstatistikkSiste4KvartalRepository(val dataSource: DataSource) 
     fun hentTotaltAntall(søkeparametere: Søkeparametere): Int? = using(sessionOf(dataSource)) { session ->
         val næringsgrupperMedBransjer = søkeparametere.næringsgrupperMedBransjer()
         val sektorer = søkeparametere.sektor.map { it.kode }.toSet()
-        val tmpKommuneTabell = "kommuner"
-        val tmpNæringTabell = "naringer"
-        val tmpNavIdenterTabell = "nav_identer"
-        val tmpSektorTabell = "sektorer"
+
         val sql = """
-                        WITH 
-                            ${filterVerdi(tmpKommuneTabell, søkeparametere.kommunenummer)},
-                            ${filterVerdi(tmpNæringTabell, næringsgrupperMedBransjer)},
-                            ${filterVerdi(tmpNavIdenterTabell, søkeparametere.navIdenter)},
-                            ${filterVerdi(tmpSektorTabell, sektorer)}
-                        SELECT
-                            COUNT(DISTINCT virksomhet.orgnr) AS total
-                        ${
-            filter(
-                tmpKommuneTabell = tmpKommuneTabell,
-                tmpNavIdenterTabell = tmpNavIdenterTabell,
-                tmpNæringTabell = tmpNæringTabell,
-                tmpSektorTabell = tmpSektorTabell,
-                søkeparametere = søkeparametere
-            )
-        }
+            SELECT
+                COUNT(DISTINCT virksomhet.orgnr) AS total
+            FROM 
+                sykefravar_statistikk_virksomhet AS statistikk
+                JOIN virksomhet USING (orgnr)
+                JOIN sykefravar_statistikk_virksomhet_siste_4_kvartal AS statistikk_siste4 USING (orgnr)
+                ${
+                    if (sektorer.isNotEmpty()) " LEFT JOIN virksomhet_statistikk_metadata USING (orgnr) "
+                    else ""
+                }
+                LEFT JOIN ia_sak ON (
+                    (ia_sak.orgnr = statistikk.orgnr) AND
+                    ia_sak.endret = (select max(endret) from ia_sak iasak2 where iasak2.orgnr = statistikk.orgnr)
+                )
+                ${
+                    if (næringsgrupperMedBransjer.isNotEmpty()) " JOIN virksomhet_naring AS vn on (virksomhet.id = vn.virksomhet) "
+                    else ""
+                }
+                
+            WHERE 
+                statistikk.arstall = :arstall
+                AND statistikk.kvartal = :kvartal
+                
+                ${filtrerPåBransjeOgNæring(søkeparametere = søkeparametere)}
+                ${filtrerPåKommuner(søkeparametere = søkeparametere)}
+                ${filtrerPåStatus(søkeparametere = søkeparametere)}
+                ${filtrerPåSektor(søkeparametere = søkeparametere)}
+                ${filtrerPåEiere(søkeparametere = søkeparametere)}
+                
+                ${søkeparametere.sykefraværsprosentFra?.let { " AND statistikk_siste4.prosent >= $it " } ?: ""}
+                ${søkeparametere.sykefraværsprosentTil?.let { " AND statistikk_siste4.prosent <= $it " } ?: ""}
+                
+                ${søkeparametere.ansatteFra?.let { " AND statistikk.antall_personer >= $it " } ?: ""}
+                ${søkeparametere.ansatteTil?.let { " AND statistikk.antall_personer <= $it " } ?: ""}
+                
+                AND virksomhet.status = '${VirksomhetStatus.AKTIV.name}'
                     """.trimIndent()
 
         val query = queryOf(
-            statement = sql, mapOf(
-                tmpKommuneTabell to session.connection.underlying.createArrayOf(
-                    "text", søkeparametere.kommunenummer.toTypedArray()
-                ),
-                tmpNæringTabell to session.connection.underlying.createArrayOf(
-                    "text", næringsgrupperMedBransjer.toTypedArray()
-                ),
-                tmpNavIdenterTabell to session.connection.underlying.createArrayOf(
-                    "text", søkeparametere.navIdenter.toTypedArray()
-                ),
-                tmpSektorTabell to session.connection.underlying.createArrayOf(
-                    "text", sektorer.toTypedArray()
-                ),
-                "kvartal" to søkeparametere.periode.kvartal, "arstall" to søkeparametere.periode.årstall
+            statement = sql,
+            mapOf(
+                "kvartal" to søkeparametere.periode.kvartal,
+                "arstall" to søkeparametere.periode.årstall,
+
+                "naringer" to session.createArrayOf("text", næringsgrupperMedBransjer),
+                "kommuner" to session.createArrayOf("text", søkeparametere.kommunenummer),
+                "sektorer" to session.createArrayOf("text", sektorer),
+                "eiere" to session.createArrayOf("text", søkeparametere.navIdenter),
             )
         )
         session.run(query.map { it.int("total") }.asSingle)
     }
-
-    private fun filter(
-        tmpKommuneTabell: String,
-        tmpNavIdenterTabell: String,
-        tmpNæringTabell: String,
-        tmpSektorTabell: String,
-        søkeparametere: Søkeparametere,
-    ) = """
-        FROM sykefravar_statistikk_virksomhet AS statistikk
-        JOIN virksomhet USING (orgnr)
-        LEFT JOIN sykefravar_statistikk_virksomhet_siste_4_kvartal AS statistikk_siste4 USING (orgnr)
-        LEFT JOIN virksomhet_statistikk_metadata USING (orgnr)
-        LEFT JOIN ia_sak ON (
-            (ia_sak.orgnr = statistikk.orgnr) AND
-            ia_sak.endret = (select max(endret) from ia_sak iasak2 where iasak2.orgnr = statistikk.orgnr)
-        )
-        JOIN virksomhet_naring AS vn on (virksomhet.id = vn.virksomhet)
-        
-        WHERE (
-            (SELECT inkluderAlle FROM $tmpKommuneTabell) IS TRUE OR
-            virksomhet.kommunenummer in (select unnest($tmpKommuneTabell.filterverdi) FROM $tmpKommuneTabell)
-        )
-        AND (
-            (SELECT inkluderAlle FROM $tmpNavIdenterTabell) IS TRUE OR
-            ia_sak.eid_av in (select unnest($tmpNavIdenterTabell.filterverdi) FROM $tmpNavIdenterTabell)
-        )
-        AND (
-            (SELECT inkluderAlle FROM $tmpSektorTabell) IS TRUE OR
-            virksomhet_statistikk_metadata.sektor in (select unnest($tmpSektorTabell.filterverdi) FROM $tmpSektorTabell)
-        )
-        AND (
-            (SELECT inkluderAlle FROM $tmpNæringTabell) IS TRUE
-            OR substr(vn.narings_kode, 1, 2) in (select unnest($tmpNæringTabell.filterverdi) FROM $tmpNæringTabell)
-            ${
-        if (søkeparametere.bransjeprogram.isNotEmpty()) {
-            val koder = søkeparametere.bransjeprogram.flatMap { it.næringskoder }.groupBy {
-                it.length
-            }
-            val femsifrede = koder[5]?.joinToString { "'${it.take(2)}.${it.takeLast(3)}'" }
-            femsifrede?.let { "OR (vn.narings_kode in (select unnest($tmpNæringTabell.filterverdi) FROM $tmpNæringTabell))" } ?: ""
-        } else ""
-    }
-        )
-        AND statistikk.kvartal = :kvartal
-        AND statistikk.arstall = :arstall
-                        
-        ${
-        søkeparametere.status?.let { status ->
-            when (status) {
-                IAProsessStatus.IKKE_AKTIV -> " AND (ia_sak.status IS NULL " +
-                        "OR ((ia_sak.status = 'IKKE_AKTUELL' OR ia_sak.status = 'FULLFØRT' OR ia_sak.status = 'SLETTET') " +
-                        "AND ia_sak.endret < '${LocalDate.now().minusDays(ANTALL_DAGER_FØR_SAK_LÅSES)}'))"
-
-                IAProsessStatus.IKKE_AKTUELL, IAProsessStatus.FULLFØRT, IAProsessStatus.SLETTET ->
-                    " AND ia_sak.status = '$status' " +
-                            "AND ia_sak.endret >= '${LocalDate.now().minusDays(ANTALL_DAGER_FØR_SAK_LÅSES)}'"
-
-                else -> " AND ia_sak.status = '$status'"
-            }
-        } ?: ""
-    }
-                        
-        ${søkeparametere.sykefraværsprosentFra?.let { " AND statistikk_siste4.prosent >= $it " } ?: ""}
-        ${søkeparametere.sykefraværsprosentTil?.let { " AND statistikk_siste4.prosent <= $it " } ?: ""}
-        
-        ${søkeparametere.ansatteFra?.let { " AND statistikk.antall_personer >= $it " } ?: ""}
-        ${søkeparametere.ansatteTil?.let { " AND statistikk.antall_personer <= $it " } ?: ""}
-        AND virksomhet.status = '${VirksomhetStatus.AKTIV.name}'
-        """.trimIndent()
-
 
     fun hentSykefraværForVirksomhet(orgnr: String): List<SykefraversstatistikkVirksomhet> {
         return using(sessionOf(dataSource)) { session ->
@@ -297,15 +300,6 @@ class SykefraværsstatistikkSiste4KvartalRepository(val dataSource: DataSource) 
             session.run(query)
         }
     }
-
-    private fun filterVerdi(filterNavn: String, filterVerdier: Set<String>) = """
-            $filterNavn (inkluderAlle, filterverdi) AS (
-                    VALUES (
-                        ${filterVerdier.isEmpty()},
-                        :$filterNavn
-                    )    
-                )
-        """.trimIndent()
 
     private fun mapRow(row: Row): SykefraversstatistikkVirksomhet {
         return SykefraversstatistikkVirksomhet(
