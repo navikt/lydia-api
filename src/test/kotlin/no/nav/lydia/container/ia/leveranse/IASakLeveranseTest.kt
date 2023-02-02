9afadb6f609617f1e1680a4fe98ae7e509e8a5c9
Christian Klem <christian.klem@nav.no>
2023-02-02 14:52:35 +0100

    Tester uthenting av leveranser
    Co-authored-by: Per-Christian Nielsen <per-christian.nielsen@nav.no>
    Co-authored-by: Ingrid Fosså <ingrid.fossa@nav.no>
    Co-authored-by: Nima Jimale <nima.jimale@nav.no>

--- src/test/kotlin/no/nav/lydia/container/ia/leveranse/IASakLeveranseTest.kt
+++ src/test/kotlin/no/nav/lydia/container/ia/leveranse/IASakLeveranseTest.kt
@@ -1,12 +1,15 @@
 package no.nav.lydia.container.ia.leveranse
 
+import io.kotest.matchers.collections.shouldHaveSize
 import io.kotest.matchers.shouldBe
 import io.ktor.http.*
 import kotlinx.datetime.toKotlinLocalDate
+import no.nav.lydia.helper.SakHelper.Companion.hentIASakLeveranser
 import no.nav.lydia.helper.SakHelper.Companion.nyHendelse
 import no.nav.lydia.helper.SakHelper.Companion.opprettLeveranse
 import no.nav.lydia.helper.SakHelper.Companion.opprettSakForVirksomhet
 import no.nav.lydia.helper.VirksomhetHelper.Companion.nyttOrgnummer
+import no.nav.lydia.helper.forExactlyOne
 import no.nav.lydia.helper.statuskode
 import no.nav.lydia.ia.sak.domene.IAProsessStatus
 import no.nav.lydia.ia.sak.domene.IASakshendelseType.*
@@ -14,8 +17,6 @@ import no.nav.lydia.ia.sak.domene.LeveranseStatus
 import kotlin.test.Test
 
 class IASakLeveranseTest {
-
-
     @Test
     fun `skal ikke kunne legge til leveranser dersom en sak ikke er i status Vi Bistår`() {
         val sakIStatusKartlegges = opprettSakForVirksomhet(orgnummer = nyttOrgnummer())
@@ -50,5 +51,37 @@ class IASakLeveranseTest {
         leveranse.saksnummer shouldBe sakIStatusViBistår.saksnummer
         leveranse.status shouldBe LeveranseStatus.UNDER_ARBEID
         leveranse.frist shouldBe frist
+    }
+
+    @Test
+    fun `skal kunne hente ut leveranser på sak`() {
+        val nå = java.time.LocalDate.now().toKotlinLocalDate()
+        val imorgen = java.time.LocalDate.now().plusDays(1).toKotlinLocalDate()
+
+        val sakIStatusViBistår = opprettSakForVirksomhet(orgnummer = nyttOrgnummer())
+            .nyHendelse(TA_EIERSKAP_I_SAK)
+            .nyHendelse(VIRKSOMHET_SKAL_KONTAKTES)
+            .nyHendelse(VIRKSOMHET_KARTLEGGES)
+            .nyHendelse(VIRKSOMHET_SKAL_BISTÅS)
+
+        sakIStatusViBistår.opprettLeveranse(
+            frist = nå,
+            modulId = 1
+        )
+        sakIStatusViBistår.opprettLeveranse(
+            frist = imorgen,
+            modulId = 2
+        )
+
+        val leveranser = hentIASakLeveranser(saksnummer = sakIStatusViBistår.saksnummer)
+        leveranser shouldHaveSize 2
+        leveranser.forExactlyOne {
+            it.frist shouldBe nå
+            it.modul.id shouldBe 1
+        }
+        leveranser.forExactlyOne {
+            it.frist shouldBe imorgen
+            it.modul.id shouldBe 2
+        }
     }
 }
