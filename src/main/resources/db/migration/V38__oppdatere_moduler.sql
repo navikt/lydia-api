eea4164d9c8c68bcb138da3fbb851e9e11e46599
Ingrid Fosså <fossa.ingrid@gmail.com>
2023-03-01 15:40:01 +0100

    Oppdaterar modular for Leveranser etter tilbakemelding frå brukarar og fagkontoret
    Fjernar: Bedriftstiltaket
    Legg til:
    - Muskel- og skjelett
    - Smertemestring og arbeidsmiljø
    - Psykis helse
    - Sees i morgen
    
    Co-authored-by: Thomas Dufourd <thomas.dufourd@nav.no>
    Co-authored-by: Nima Jimale <nima.jimale@nav.no>

---
+++ src/main/resources/db/migration/V38__oppdatere_moduler.sql
@@ -0,0 +1,6 @@
+update modul set navn = 'Muskel- og skjelett' where id = 11 and navn = 'Bedriftstiltaket';
+
+insert into modul (id, ia_tjeneste, navn) VALUES
+    (12, 3,'Smertemestring og arbeidsmiljø');
+    (13, 3,'Psykisk helse');
+    (14, 3,'Sees i morgen');
