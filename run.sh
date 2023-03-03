78cb97f99c4d9801f80da1325ee7f6590116a921
Thomas Dufourd <thomas.dufourd@gmail.com>
2023-03-03 12:06:48 +0100

    Oppdater script for å kjøre applikasjonen lokalt med ny sql dump fil

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 ./gradlew build -x test
 docker-compose up -d
-curl -o db_script.sql https://raw.githubusercontent.com/navikt/lydia-api/main/scripts/db/lydia_api_container_db_localhost-2023_01_11_13_11_11-dump.sql
+curl -o db_script.sql https://raw.githubusercontent.com/navikt/lydia-api/main/scripts/db/lydia_api_container_db_localhost-2023_03_03_08_52_50-dump.sql
 sleep 3
 PGPASSWORD=test psql -h localhost -p 5432 -U postgres -f db_script.sql > /dev/null
 rm db_script.sql
