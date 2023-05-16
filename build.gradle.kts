956d0f7019d0a28bcde1182053443a21584d9d05
Christian Klem <christian.klem@nav.no>
2023-05-16 08:59:49 +0200

    Oppdater micrometer-registry-prometheus 1.10.5 -> 1.11.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,7 +45,7 @@ dependencies {
 
     // metrics
     implementation("io.ktor:ktor-server-metrics-micrometer:$ktorVersion")
-    implementation("io.micrometer:micrometer-registry-prometheus:1.10.5")
+    implementation("io.micrometer:micrometer-registry-prometheus:1.11.0")
 
     // Database
     implementation("org.postgresql:postgresql:42.6.0")
