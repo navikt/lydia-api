84277b07da57e88927aeae8e1dcac4bd4fd510d9
Christian Klem <christian.klem@nav.no>
2023-06-05 16:02:43 +0200

    Oppdater testcontainers - 1.18.1 -> 1.18.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -86,7 +86,7 @@ dependencies {
     testImplementation("io.kotest:kotest-assertions-json:$kotestVerstion")
 
     // Testcontainers
-    val testcontainersVersion = "1.18.1"
+    val testcontainersVersion = "1.18.3"
     testImplementation("org.testcontainers:testcontainers:$testcontainersVersion")
     testImplementation("org.testcontainers:junit-jupiter:$testcontainersVersion")
     testImplementation("org.testcontainers:postgresql:$testcontainersVersion")
