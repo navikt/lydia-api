ce8f9c8436f6b38aa153ee910583b2ba816fc195
Christian Klem <christian.klem@nav.no>
2023-06-23 11:10:01 +0200

    Hent sykefraværsstatistikkperiode for hendelser basert på publiseringsdato ved re-eksport
    Co-authored-by: Per-Christian Nielsen <per-christian.nielsen@nav.no>

--- src/main/kotlin/no/nav/lydia/sykefraversstatistikk/SistePubliseringRepository.kt
+++ src/main/kotlin/no/nav/lydia/sykefraversstatistikk/SistePubliseringRepository.kt
@@ -23,6 +23,19 @@ class SistePubliseringRepository(val dataSource: DataSource) {
             ) ?: throw NoSuchElementException("Ingen publiseringsinfo funnet")
         }
 
+    fun hentAllPubliseringsinfo() =
+        using(sessionOf(dataSource)) { session ->
+            session.run(
+                queryOf(
+                    """
+                        SELECT *
+                        FROM siste_publiseringsinfo
+                        ORDER BY gjeldende_arstall DESC, gjeldende_kvartal DESC
+                    """.trimMargin()
+                ).map(this::mapRowToPubliseringsinfo).asList
+            )
+        }
+
     private fun mapRowToPubliseringsinfo(row: Row): PubliseringsinfoDto {
         return row.tilPubliseringsinfo()
     }
