e9a37ef00312a1e74f05e096ebeb36e96d1c9039
Christian Klem <christian.klem@nav.no>
2023-06-26 13:10:56 +0200

    Fjern migrering av siste_publiseringsinfo - kun for test av bygg
    Co-authored-by: Per-Christian Nielsen <per-christian.nielsen@nav.no>

--- src/main/resources/db/migration/V46__populer_siste_publiseringsdato.sql
+++
@@ -1,5 +0,0 @@
-insert into siste_publiseringsinfo(gjeldende_arstall, gjeldende_kvartal, siste_publiseringsdato, neste_publiseringsdato) values (2022, 3, '2022-12-01', '2023-03-02');
-insert into siste_publiseringsinfo(gjeldende_arstall, gjeldende_kvartal, siste_publiseringsdato, neste_publiseringsdato) values (2022, 2, '2022-09-08', '2022-12-01');
-insert into siste_publiseringsinfo(gjeldende_arstall, gjeldende_kvartal, siste_publiseringsdato, neste_publiseringsdato) values (2022, 1, '2022-06-02', '2022-09-08');
-insert into siste_publiseringsinfo(gjeldende_arstall, gjeldende_kvartal, siste_publiseringsdato, neste_publiseringsdato) values (2021, 4, '2022-03-03', '2022-06-02');
-insert into siste_publiseringsinfo(gjeldende_arstall, gjeldende_kvartal, siste_publiseringsdato, neste_publiseringsdato) values (2021, 3, '2021-12-02', '2022-03-03');
