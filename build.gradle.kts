c2f0227336ca547d358043ab79c85366f09720ae
Thomas Dufourd <thomas.dufourd@gmail.com>
2023-07-18 15:24:12 +0200

    Oppdater mock-oauth2-server til neste minor versjon

--- build.gradle.kts
+++ build.gradle.kts
@@ -95,7 +95,7 @@ dependencies {
 
 
     // Autentisering
-    testImplementation("no.nav.security:mock-oauth2-server:0.5.9")
+    testImplementation("no.nav.security:mock-oauth2-server:0.5.10")
     implementation("com.nimbusds:nimbus-jose-jwt:9.31")
 
     constraints {
