382a69dd30ead075f4b383348a25f61c6aff1d50
Christian Klem <christian.klem@nav.no>
2023-09-11 12:09:39 +0200

    Oppdater wiremock 2.35.0 -> 3.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -90,7 +90,7 @@ dependencies {
 
     // Http-mocking
     testImplementation("com.github.kittinunf.fuel:fuel-kotlinx-serialization:$fuelVersion")
-    testImplementation("com.github.tomakehurst:wiremock-jre8-standalone:2.35.0")
+    testImplementation("com.github.tomakehurst:wiremock-standalone:3.0.1")
 
 
     // Autentisering
