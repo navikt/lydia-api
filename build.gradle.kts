e3c0030e6b1e45316055f82fafbd3676acb86a48
Christian Klem <christian.klem@nav.no>
2023-09-20 14:42:21 +0200

    Oppdater mock-oauth-server 1.0.0 -> 2.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -91,7 +91,7 @@ dependencies {
 
 
     // Autentisering
-    testImplementation("no.nav.security:mock-oauth2-server:1.0.0")
+    testImplementation("no.nav.security:mock-oauth2-server:2.0.0")
     implementation("com.nimbusds:nimbus-jose-jwt:9.31")
 
     constraints {
