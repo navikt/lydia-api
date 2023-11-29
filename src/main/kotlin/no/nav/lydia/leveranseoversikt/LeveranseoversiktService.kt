1de3f2c92f1c8b3ce230ba05a83e59f48d7843f5
Ingrid Fosså <fossa.ingrid@gmail.com>
2023-11-29 12:10:11 +0100

    Gjev testfila vår eit betre namn
    Co-authored-by: Christian Klem <christian.klem@nav.no>

--- src/main/kotlin/no/nav/lydia/leveranseoversikt/LeveranseoversiktService.kt
+++ src/main/kotlin/no/nav/lydia/leveranseoversikt/LeveranseoversiktService.kt
@@ -9,17 +9,13 @@ import org.slf4j.LoggerFactory
 class LeveranseoversiktService(val leveranseoversiktRepository: LeveranseoversiktRepository) {
     val log = LoggerFactory.getLogger(this.javaClass)
 
-
     fun hentMineLeveranser(saksbehandler: NavAnsatt.NavAnsattMedSaksbehandlerRolle): Either<Feil, List<LeveranseoversiktDto>> {
         return try {
-
             Either.Right(leveranseoversiktRepository.hentLeveranser(saksbehandler = saksbehandler))
         } catch (e: Exception) {
             log.error("Kunne ikke hente leveranser for bruker. Feilmelding: ${e.message}", e)
             Either.Left(Feil("Kunne ikke hente leveransene dine", HttpStatusCode.InternalServerError))
         }
     }
-
-
 
 }
