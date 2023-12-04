55fff8f7527ad9b8df907eeaae024017efaadfa9
Christian Klem <christian.klem@nav.no>
2023-12-04 16:54:44 +0100

    Oppdater kotlin 1.9.20 -> 1.9.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
-    kotlin("jvm") version "1.9.20"
+    kotlin("jvm") version "1.9.21"
     // Skru json-serialisering
-    kotlin("plugin.serialization") version "1.9.20"
+    kotlin("plugin.serialization") version "1.9.21"
     // For å bygge fatjar
     id("com.github.johnrengelman.shadow") version "8.1.1"
     // Apply the application plugin to add support for building a CLI application in Java.
