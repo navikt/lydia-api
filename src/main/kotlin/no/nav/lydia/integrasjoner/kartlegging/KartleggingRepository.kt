86db334b40610fec20b3341582e72dd63e3e7651
Sindre Larsen <silar.dev@proton.me>
2024-01-24 09:43:31 +0100

    Svar lagres i db og overskrives ved nytt svar
    Co-authored-by: Thomas Dufourd <thomas.dufourd@nav.no>
    Co-authored-by: Per-Christian Nielsen <per-christian.nielsen@nav.no>

--- src/main/kotlin/no/nav/lydia/integrasjoner/kartlegging/KartleggingRepository.kt
+++ src/main/kotlin/no/nav/lydia/integrasjoner/kartlegging/KartleggingRepository.kt
@@ -11,19 +11,22 @@ class KartleggingRepository(val dataSource: DataSource) {
             session.run(
                 queryOf(
                     """
-                    INSERT INTO ia_sak_kartlegging_svar (
-                        kartlegging_id,
-                        sesjon_id,
-                        sporsmal_id,
-                        svar_id
-                    )
-                    VALUES (
-                        :kartleggingId,
-                        :sesjonId,
-                        :sporsmalId,
-                        :svarId
-                  )
-                """.trimMargin(),
+                        INSERT INTO ia_sak_kartlegging_svar (
+                            kartlegging_id,
+                            sesjon_id,
+                            sporsmal_id,
+                            svar_id
+                        )
+                        VALUES (
+                            :kartleggingId,
+                            :sesjonId,
+                            :sporsmalId,
+                            :svarId
+                        )
+                        ON CONFLICT ON CONSTRAINT ia_sak_kartlegging_svar_kartlegging_sesjon_spm DO UPDATE SET
+                            svar_id = :svarId,
+                            endret = now()
+                        """.trimMargin(),
                     mapOf(
                         "kartleggingId" to karleggingSvarDto.kartleggingId,
                         "sesjonId" to karleggingSvarDto.sesjonId,
