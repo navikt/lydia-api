f6d1597fdc442a343870843d40b754190154649c
Sindre Larsen <silar.dev@proton.me>
2024-01-25 15:25:41 +0100

    Nye tabeller og hardkodede spørsmål og svar
    Co-authored-by: Thomas Dufourd <thomas.dufourd@nav.no>

---
+++ src/main/resources/db/migration/2024/V70__sporsmal_og_svar_relasjoner.sql
@@ -0,0 +1,23 @@
+-- spm 1
+INSERT INTO ia_sak_kartlegging_svaralternativer_til_sporsmal (sporsmal_id, svaralternativ_id)
+VALUES ('b16c4b1c-b45e-470d-a1a5-d6f87424d410', '9d0f4376-b9b2-49f9-8155-a21f525e569d'),
+       ('b16c4b1c-b45e-470d-a1a5-d6f87424d410', '60ec8889-3a36-42b8-b877-a84dfd53e2a8'),
+       ('b16c4b1c-b45e-470d-a1a5-d6f87424d410', 'cafb822f-1f1d-4630-8a07-b919223b6a23'),
+       ('b16c4b1c-b45e-470d-a1a5-d6f87424d410', '47737966-cc37-4d1b-bef9-9c47fb5ba832'),
+       ('b16c4b1c-b45e-470d-a1a5-d6f87424d410', '9d39b9a6-2c48-435b-9cd4-3a174c5f4edd');
+
+-- spm 2
+INSERT INTO ia_sak_kartlegging_svaralternativer_til_sporsmal (sporsmal_id, svaralternativ_id)
+VALUES ('61a9a84a-949b-4f46-97e5-c9b60e01d433', '5077051a-c781-458f-a197-5fedcb1bdb37'),
+       ('61a9a84a-949b-4f46-97e5-c9b60e01d433', 'c0ad8503-7b99-4263-a771-7c76b73810fd'),
+       ('61a9a84a-949b-4f46-97e5-c9b60e01d433', 'd3ed541e-9acc-4a50-9bd7-6cb952df7089'),
+       ('61a9a84a-949b-4f46-97e5-c9b60e01d433', '6aaa4b14-4c53-4659-92c0-b763ad80707e'),
+       ('61a9a84a-949b-4f46-97e5-c9b60e01d433', 'c73c7662-ea3a-4056-a864-9f117cecafac');
+
+-- spm 3
+INSERT INTO ia_sak_kartlegging_svaralternativer_til_sporsmal (sporsmal_id, svaralternativ_id)
+VALUES ('62b3a863-cba3-4c92-8c7e-19d8b4688d49', 'fa45b7b6-bfa6-493c-99e3-176058f00e13'),
+       ('62b3a863-cba3-4c92-8c7e-19d8b4688d49', 'fc3830dd-fd32-4dca-9ee0-61b8b518b9b4'),
+       ('62b3a863-cba3-4c92-8c7e-19d8b4688d49', '8747734d-99b3-4952-9274-2bbdd2e5a831'),
+       ('62b3a863-cba3-4c92-8c7e-19d8b4688d49', 'a8576fe8-683c-4655-979e-3eafe0b52206'),
+       ('62b3a863-cba3-4c92-8c7e-19d8b4688d49', 'afb5e00c-59d5-4343-9b04-f9093c904e8a');
