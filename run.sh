f9a7ba07b7828bd21c11147f5ce225a790a37a40
nimajimale <nima0512@gmail.com>
2024-02-29 14:45:14 +0100

    Ny dump av testdata etter publisering av statistikk
    Co-authored-by: Thomas Dufourd <thomas.dufourd@nav.no>
    Co-authored-by: Per-Christian Nielsen <per-christian.nielsen@nav.no>

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 ./gradlew build -x test
 docker-compose up -d
-curl -o db_script.sql https://raw.githubusercontent.com/navikt/lydia-api/main/scripts/db/lydia-api-container-db_localhost-2023_12_01_13_01_04-dump.sql
+curl -o db_script.sql https://raw.githubusercontent.com/navikt/lydia-api/main/scripts/db/lydia-api-container-db_localhost-2024_02_29_14_42_55-dump.sql
 sleep 3
 PGPASSWORD=test psql -h localhost -p 5432 -U postgres -f db_script.sql > /dev/null
 rm db_script.sql
