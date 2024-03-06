82fbb822ac5880ce793b61a745929acd4d636305
Christian Klem <christian.klem@nav.no>
2024-03-06 10:05:59 +0100

    Fyr av leveranse-eksport som en bakgrunnsjobb
    Co-authored-by: Per-Christian Nielsen <per-christian.nielsen@nav.no>

--- src/main/kotlin/no/nav/lydia/ia/eksport/IASakLeveranseEksportør.kt
+++ src/main/kotlin/no/nav/lydia/ia/eksport/IASakLeveranseEksportør.kt
@@ -1,28 +1,25 @@
 package no.nav.lydia.ia.eksport
 
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 import no.nav.lydia.ia.sak.db.IASakLeveranseRepository
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import java.util.concurrent.atomic.AtomicBoolean
 
 class IASakLeveranseEksportør(
     val iaSakLeveranseRepository: IASakLeveranseRepository,
     val iaSakLeveranseProdusent: IASakLeveranseProdusent,
 ) {
-    companion object {
-        val KJØRER_LEVERANSE_EKSPORT = AtomicBoolean(false)
-    }
-
     val log: Logger = LoggerFactory.getLogger(this.javaClass)
 
-    fun eksporter() {
-        KJØRER_LEVERANSE_EKSPORT.set(true)
-        val alleLeveranser = iaSakLeveranseRepository.hentAlleIASakLeveranser()
-        log.info("Starter re-eksport av ${alleLeveranser.size} leveranser")
-        alleLeveranser.forEach {
-            iaSakLeveranseProdusent.receive(it)
+    suspend fun eksporter() = coroutineScope {
+        launch {
+            val alleLeveranser = iaSakLeveranseRepository.hentAlleIASakLeveranser()
+            log.info("Starter re-eksport av ${alleLeveranser.size} leveranser")
+            alleLeveranser.forEach {
+                iaSakLeveranseProdusent.receive(it)
+            }
+            log.info("Ferdig med re-eksport av ${alleLeveranser.size} leveranser")
         }
-        log.info("Ferdig med re-eksport av ${alleLeveranser.size} leveranser")
-        KJØRER_LEVERANSE_EKSPORT.set(false)
     }
 }
