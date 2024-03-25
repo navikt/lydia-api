b1cf12a82522e055eef21ebd586fa0393212cb87
Thomas Dufourd <thomas.dufourd@gmail.com>
2024-03-25 10:04:20 +0100

    Oppdatere introtekst til tema 'Redusere sykefravær'
    Co-authored-by: Nima Jimale <nima.jimale@nav.no>
    Co-authored-by: Per-Christian Nielsen <per-christian.nielsen@nav.no>

---
+++ src/main/resources/db/migration/2024/V75__oppdater_introtekst_for_redusere_sykefravar.sql
@@ -0,0 +1,3 @@
+UPDATE ia_sak_kartlegging_tema
+SET introtekst = 'Sykefravær er en viktig indikator for arbeidsmiljøet og ansattes helse. Å ha et bevisst forhold til sykefraværsarbeid på arbeidsplassen er avgjørende for å skape et trivelig og produktivt arbeidsmiljø. Gjennom kartlegging kan man identifisere mønstre, sette mål og implementere tiltak for å redusere sykefraværet. Dette bidrar til et bedre arbeidsmiljø og økt trivsel for alle ansatte.'
+WHERE tema_id = 2;
