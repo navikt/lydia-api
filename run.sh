5cea2a4a74a8f866a4d3d08c0fa15e1dfaab3636
Thomas Dufourd <thomas.dufourd@gmail.com>
2024-05-31 13:03:30 +0200

    Oppdater run script (lokalt) med siste DB dump (nytt kvartal til statistikk)
    Co-authored-by: Nima Jimale <nima.jimale@nav.no>

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 ./gradlew build -x test
 docker-compose up -d
-curl -o db_script.sql https://raw.githubusercontent.com/navikt/lydia-api/main/scripts/db/lydia-api-container-db_localhost-2024_02_29_14_42_55-dump.sql
+curl -o db_script.sql https://raw.githubusercontent.com/navikt/lydia-api/main/scripts/db/lydia-api-container-db_localhost-2024_05_31_12_40_54-dump.sql
 sleep 3
 PGPASSWORD=test psql -h localhost -p 5432 -U postgres -f db_script.sql > /dev/null
 rm db_script.sql
