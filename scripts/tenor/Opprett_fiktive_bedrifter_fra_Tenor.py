28af7653ee8459d8a4562866f40a6883942fc6a4
Sindre Larsen <silar.dev@proton.me>
2024-06-25 10:54:13 +0200

    Oppdaterer script til å lese fra flere søk og genere ett eller flere sql scripts

--- scripts/tenor/Opprett_fiktive_bedrifter_fra_Tenor.py
+++ scripts/tenor/Opprett_fiktive_bedrifter_fra_Tenor.py
@@ -1,97 +1,209 @@
 import json
-
-import_file = 'tenor/data/eksport-brreg-er-fr-2023-05-22T14_28_30.635Z--Sykehus--86.json'
-
-output_file_SQL = 'tenor/output/insert_fiktive_virksomheter.sql'
-output_file_fiktive_orgnr_liste_TXT = 'tenor/output/fiktive_orgnr.txt'
+import os
+import csv
 
 
-def insert_into_virksomhet(
-        orgnr,
-        navn,
-        kommune,
-        postnummer,
-        adresse_lines,
-        kommunenummer,
-        poststed,
-        oppstartsdato,
-        landkode,
-        land
+class Virksomhet:
+    def __init__(
+            self,
+            orgnummer,
+            navn,
+            næringskode,
+            kommune,
+            postnummer,
+            adresse_lines,
+            kommunenummer,
+            poststed,
+            landkode,
+            land,
+            oppstartsdato,
+            antall_ansatte
+    ):
+        self.orgnummer = orgnummer
+        self.navn = navn
+        self.næringskode = næringskode
+        self.kommune = kommune
+        self.postnummer = postnummer
+        self.adresse_lines = adresse_lines
+        self.kommunenummer = kommunenummer
+        self.poststed = poststed
+        self.landkode = landkode
+        self.land = land
+        self.oppstartsdato = oppstartsdato
+        self.antall_ansatte = antall_ansatte
+
+    def __eq__(self, other):
+        return self.orgnummer == other.orgnummer
+
+    def __hash__(self):
+        return hash(('orgnummer', self.orgnummer))
+
+
+def insert_into_virksomhet(virksomhet: Virksomhet):
+    separator = ", "
+    adresse = separator.join(f'"{adresse_line}"' for adresse_line in virksomhet.adresse_lines)
+    return (
+        f"INSERT INTO public.virksomhet "
+        f"(orgnr, land, landkode, postnummer, poststed, kommune, kommunenummer, navn, adresse, status, oppstartsdato) "
+        f"VALUES ('{virksomhet.orgnummer}', '{virksomhet.land}', '{virksomhet.landkode}', '{virksomhet.postnummer}', '{virksomhet.poststed}', '{virksomhet.kommune}', '{virksomhet.kommunenummer}', "
+        f"'{virksomhet.navn}', '{{{adresse}}}', "
+        f"'AKTIV', '{virksomhet.oppstartsdato}') ON CONFLICT DO NOTHING;"
+    )
+
+
+def insert_into_virksomhet_naringsundergrupper(orgnr: str, naringskode: str) -> str:
+    return (
+        f"INSERT INTO public.virksomhet_naringsundergrupper "
+        f" (virksomhet, naringsundergruppe1) "
+        f"select id, '{naringskode}' as kode from virksomhet where orgnr='{orgnr}' ON CONFLICT DO NOTHING; "
+    )
+
+
+def ansatteTilEnum(antall_ansatte):
+    if antall_ansatte <= 25:
+        return "LITEN"
+    elif antall_ansatte <= 100:
+        return "MEDIUM"
+    elif antall_ansatte <= 10000:
+        return "STOR"
+    else:
+        return "ENORM"
+
+
+def parseTenorFil(
+        import_filename: str,
+) -> list[Virksomhet]:
+    nye_virksomheter = []
+
+    with open(import_filename, "r") as tenorTestDataFil:
+        json_decode = json.load(tenorTestDataFil)
+        for virksomhet in json_decode["dokumentListe"]:
+            nye_virksomheter.append(
+                Virksomhet(
+                    orgnummer=virksomhet.get("organisasjonsnummer"),
+                    navn=virksomhet.get("navn"),
+                    næringskode=virksomhet.get("naeringKode")[0],
+                    kommune=virksomhet.get("forretningsadresse").get("kommune"),
+                    postnummer=virksomhet.get("forretningsadresse").get("postnummer"),
+                    adresse_lines=[adresse.replace("'", "") for adresse in
+                                   virksomhet.get("forretningsadresse").get("adresse")],
+                    kommunenummer=virksomhet.get("forretningsadresse").get("kommunenummer"),
+                    poststed=virksomhet.get("forretningsadresse").get("poststed"),
+                    landkode=virksomhet.get("forretningsadresse").get("landkode"),
+                    land=virksomhet.get("forretningsadresse").get("land"),
+                    oppstartsdato=virksomhet.get("registreringsdatoEnhetsregisteret"),
+                    antall_ansatte=virksomhet.get("antallAnsatte")
+                )
+            )
+    return nye_virksomheter
+
+
+def skriv_til_fil(
+        output_filename_SQL: str,
+        output_filename_fiktive_orgnr_liste_csv: str,
+        nye_virksomheter: list[Virksomhet]
 ):
-    separator = ', '
-    adresse = separator.join(f'"{adresse_line}"' for adresse_line in adresse_lines)
-    return f"INSERT INTO public.virksomhet " \
-           f"(orgnr, land, landkode, postnummer, poststed, kommune, kommunenummer, navn, adresse, status, oppstartsdato) " \
-           f"VALUES ('{orgnr}', '{land}', '{landkode}', '{postnummer}', '{poststed}', '{kommune}', '{kommunenummer}', " \
-           f"'{navn}', '{{{adresse}}}', " \
-           f"'AKTIV', '{oppstartsdato}');"
-
-
-def insert_into_virksomhet_naringsundegrupper(orgnr, naringskode):
-    return f"INSERT INTO public.virksomhet_naringsundergrupper " \
-           f" (virksomhet, naringsundergruppe1) " \
-           f"select id, '{naringskode}' as kode from virksomhet where orgnr='{orgnr}'; "
-
-
-with open(import_file, 'r') as tenorTestDataFil, \
-        open(output_file_SQL, 'w') as output_file_inserts_sql, \
-        open(output_file_fiktive_orgnr_liste_TXT, 'w') as output_file_liste_av_fiktive_orgnr:
-    json_decode = json.load(tenorTestDataFil)
-    result = []
-    for item in json_decode['dokumentListe']:
-        organisasjonner = {
-            'orgnr': item.get('organisasjonsnummer'),
-            'navn': item.get('navn'),
-            'organisasjonsform': item.get('organisasjonsform').get('kode'),
-            'naringskode': item.get('naeringKode')[0],
-            'forretningsadresse': item.get('forretningsadresse'),
-            'registreringsdatoEnhetsregisteret': item.get('registreringsdatoEnhetsregisteret')
-        }
-        result.append(organisasjonner)
-
-    antall_fiktive_virksomheter = 0
-    for item in result:
-        kommune = item.get('forretningsadresse').get('kommune')
-        postnummer = item.get('forretningsadresse').get('postnummer')
-        adresse_lines: list = item.get('forretningsadresse').get('adresse')
-        kommunenummer = item.get('forretningsadresse').get('kommunenummer')
-        poststed = item.get('forretningsadresse').get('poststed')
-        landkode = item.get('forretningsadresse').get('landkode')
-        land = item.get('forretningsadresse').get('land')
-        oppstartsdato = item.get('registreringsdatoEnhetsregisteret')
-
-        # orgnr, navn, kommune, postnummer, adresse_lines, kommunenummer, poststed, landkode, land
-        output_file_inserts_sql.write(
-            insert_into_virksomhet(
-                item.get('orgnr'),
-                item.get('navn'),
-                kommune,
-                postnummer,
-                adresse_lines,
-                kommunenummer,
-                poststed,
-                oppstartsdato,
-                landkode,
-                land
-            )
+    with (open(output_filename_SQL, "w") as sql_file,
+          open(output_filename_fiktive_orgnr_liste_csv, 'w') as csv_file):
+        csv_builder = csv.writer(csv_file)
+        csv_builder.writerow(
+            ['orgnr',
+             'sektor',
+             'primærnæring',
+             'størrelse']
         )
-        output_file_inserts_sql.write('\n')
-        output_file_inserts_sql.write(
-            insert_into_virksomhet_naringsundergrupper(
-                item.get('orgnr'),
-                item.get('naringskode')
+
+        for indeks, ny_virksomhet in enumerate(nye_virksomheter):
+            sektor = (indeks % 3) + 1
+            størrelse = ansatteTilEnum(ny_virksomhet.antall_ansatte)
+
+            csv_builder.writerow(
+                [ny_virksomhet.orgnummer,
+                 sektor,
+                 ny_virksomhet.næringskode,
+                 størrelse,
+                 ]
             )
-        )
-        output_file_inserts_sql.write('\n')
-        output_file_liste_av_fiktive_orgnr.write(item.get('orgnr'))
-        output_file_liste_av_fiktive_orgnr.write('\n')
-    print('----> Closing files')
-    output_file_inserts_sql.close()
-    output_file_liste_av_fiktive_orgnr.close()
 
-print('----> Nye bedrifter som kan opprettes: ')
-for item in result:
-    antall_fiktive_virksomheter = antall_fiktive_virksomheter + 1
-    print(item.get('orgnr'))
+            sql_file.write(
+                insert_into_virksomhet(
+                    ny_virksomhet
+                )
+            )
+            sql_file.write("\n")
+            sql_file.write(
+                insert_into_virksomhet_naringsundergrupper(
+                    ny_virksomhet.orgnummer,
+                    ny_virksomhet.næringskode
+                )
+            )
+            sql_file.write("\n")
 
-print(f'----> Antall bedrifter: {antall_fiktive_virksomheter}')
+    return len(nye_virksomheter)
+
+
+if __name__ == "__main__":
+    input_folder = "tenor/data"
+    output_folder = "tenor/output"
+
+    alle_virksomheter = []
+    innleste_orgnr = set()
+    duplikater_fjernet = 0
+
+    lag_individuelle_mapper_og_filer = False
+
+    if not os.path.exists(output_folder):
+        os.mkdir(output_folder)
+
+    for fylke in os.listdir(input_folder):
+        if fylke == ".DS_Store":
+            continue
+
+        if not os.path.exists(f"{output_folder}/{fylke}") and lag_individuelle_mapper_og_filer:
+            os.mkdir(f"{output_folder}/{fylke}")
+
+        print(f"Antall virksomheter for {fylke}:")
+        count = 0
+
+        for næring in os.listdir(f"{input_folder}/{fylke}"):
+            if næring == ".DS_Store":
+                continue
+
+            if not os.path.exists(f"{output_folder}/{fylke}/{næring}") and lag_individuelle_mapper_og_filer:
+                os.mkdir(f"{output_folder}/{fylke}/{næring}")
+
+            for filename in os.listdir(f"{input_folder}/{fylke}/{næring}"):
+                if filename == ".DS_Store":
+                    continue
+
+                import_file = f"{input_folder}/{fylke}/{næring}/{filename}"
+
+                nye_virksomheter = parseTenorFil(
+                    import_file,
+                )
+                antall_nye_virksomheter = len(nye_virksomheter)
+                print(f" {næring}: {len(nye_virksomheter)}")
+
+                nye_virksomheter = [virksomhet for virksomhet in nye_virksomheter if
+                                    virksomhet.orgnummer not in innleste_orgnr]
+
+                innleste_orgnr.update([virksomhet.orgnummer for virksomhet in nye_virksomheter])
+
+                duplikater_fjernet += (antall_nye_virksomheter - len(nye_virksomheter))
+
+                count += len(nye_virksomheter)
+
+                if lag_individuelle_mapper_og_filer:
+                    skriv_til_fil(
+                        f"{output_folder}/{fylke}/{næring}/insert_testvirksomheter.sql",
+                        f"{output_folder}/{fylke}/{næring}/testvirksomheter.csv",
+                        nye_virksomheter
+                    )
+
+                alle_virksomheter.extend(nye_virksomheter)
+
+        print(f" Totalt: {count}")
+        print(f"---------------")
+
+    if duplikater_fjernet > 0:
+        print(f"Duplikater droppet: {duplikater_fjernet}")
--- scripts/tenor/README.md
+++ scripts/tenor/README.md
@@ -1,24 +1,33 @@
 # Opprett testdata med Tenor
+
 ## Hent testdata
-Logg inn med personlig fnr på https://testdata.skatteetaten.no/web/testnorge/soek/freg 
+
+Logg inn med egen BankId på [Tenor](https://testdata.skatteetaten.no/web/testnorge/soek/freg)
 
 Naviger til [avansert søk med KQL](https://testdata.skatteetaten.no/web/testnorge/avansert/brreg-er-fr)
 
-Limm in følgende søk i søkefeltet: 
+Lim inn f.eks. følgende søk i søkefeltet:
+
 ```erUnderenhet : * and naeringKode : 86* and antallAnsatte>= 5 ```
 
-Her ønsker vi å hente en liste av underenheter med næringskode `86.***` som har flere enn 5 ansatte. 
+I eksempelet over ønsker vi å hente en liste av underenheter med næringskode `86.***` som har flere enn 5 ansatte.
 
 ## Eksport testdata til JSON
+
 På høyresiden av KQL søkefeltet klick på de tre prikkene. I dialogboksen velg "Eksportere resultat"
 
 I neste vindu angi/velg:
- - antall bedrifter du vil eksportere
- - hukk av check-box "all informasjon inkludert tilknyttende kilder"
 
-Ta vare på filen som lastes ned på din maskin. 
+- antall bedrifter du vil eksportere
+- hukk av check-box "all informasjon inkludert tilknyttende kilder"
+
+Ta vare på filen som lastes ned på din maskin, og legg i rett mappe:
+```lydia-api/scripts/tenor/data_per_fylke/Fylke/primærnæring/din_nye_fil.json```
 
 ## Lag SQL scripts for å opprette testdata
-Kjør følgende scripts lokalt:
- - Parse_api_sykefraversstatistikk_response.py produserer en liste av orgnr basert på en json fil i `scripts/tenor/tmp/api_sykefraversstatistikk_response.json`. Filen laster du ned fra Fia i test. 
- - Opprett_fiktive_bedrifter_fra_Tenor.py lager en SQL fil som oppretter fiktive bedrifter ut i fra en json fil som lastes ned fra Tenor (se eksempler i `scripts/tenor/data` mappe)
+
+Kjør script som parser json lokalt: `python3 Opprett_fiktive_bedrifter_fra_Tenor.py`
+Dette scriptet oppretter to filer:
+
+- csv fil som skal til sykefraværsstatisikk-api for å genere sykefraværsstatistikk
+- sql fil som skal kjøres i databasen for å opprette testdata.
