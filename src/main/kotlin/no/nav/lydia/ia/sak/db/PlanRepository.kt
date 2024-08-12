ee4fba24088481d24529f1d0ec788aebfcfdd015
Sindre Larsen <silar.dev@proton.me>
2024-08-12 16:16:22 +0200

    persisterer plan

--- src/main/kotlin/no/nav/lydia/ia/sak/db/PlanRepository.kt
+++ src/main/kotlin/no/nav/lydia/ia/sak/db/PlanRepository.kt
@@ -4,11 +4,19 @@ import arrow.core.Either
 import arrow.core.left
 import arrow.core.right
 import io.ktor.http.HttpStatusCode
+import kotlinx.datetime.toKotlinLocalDate
+import kotliquery.Row
+import kotliquery.Session
+import kotliquery.queryOf
+import kotliquery.sessionOf
+import kotliquery.using
 import no.nav.lydia.ia.sak.api.Feil
+import no.nav.lydia.ia.sak.domene.plan.DUMMY_BESKRIVELSE
+import no.nav.lydia.ia.sak.domene.plan.DUMMY_MÅLSETNING
 import no.nav.lydia.ia.sak.domene.plan.Plan
+import no.nav.lydia.ia.sak.domene.plan.PlanRessurs
 import no.nav.lydia.ia.sak.domene.plan.PlanTema
 import no.nav.lydia.ia.sak.domene.plan.PlanUndertema
-import no.nav.lydia.ia.sak.domene.plan.getHardkodetPlan
 import no.nav.lydia.tilgangskontroll.fia.NavAnsatt
 import java.util.UUID
 import javax.sql.DataSource
@@ -16,37 +24,128 @@ import javax.sql.DataSource
 class PlanRepository(
     val dataSource: DataSource,
 ) {
+    private val hardkodetPlanMal: List<Pair<String, List<String>>> = listOf(
+        "Partssamarbeid" to
+            listOf(
+                "Utvikle partssamarbeidet",
+            ),
+        "Sykefraværsarbeid" to
+            listOf(
+                "Sykefraværsrutiner",
+                "Oppfølgingssamtaler",
+                "Tilretteleggings- og medvirkningsplikt",
+                "Sykefravær - enkeltsaker",
+            ),
+        "Arbeidsmiljø" to
+            listOf(
+                "Utvikle arbeidsmiljøet",
+                "Endring og omstilling",
+                "Oppfølging av arbeidsmiljøundersøkelser",
+                "Livsfaseorientert personlapolitikk",
+                "Psykisk helse",
+            ),
+        "HelseIArbeid" to
+            listOf(
+                "HelseIArbeid",
+            ),
+    )
+
     fun opprettPlan(
         planId: UUID,
         prosessId: Int,
         saksbehandler: NavAnsatt.NavAnsattMedSaksbehandlerRolle,
+        mal: List<Pair<String, List<String>>> = hardkodetPlanMal,
     ): Either<Feil, Plan> {
-//        TODO: Lagre plan på prosess
-//        using(sessionOf(dataSource)) { session ->
-//            session.transaction { tx ->
-//                tx.run(
-//                    queryOf(
-//                        """
-//                            INSERT INTO ia_sak_plan (
-//                                plan_id,
-//                                ia_prosess,
-//                                opprettet_av
-//                            )
-//                            VALUES (
-//                                :plan_id,
-//                                :prosess_id,
-//                                :opprettet_av
-//                            )
-//                        """.trimMargin(),
-//                        mapOf(
-//                            "plan_id" to planId,
-//                            "prosess_id" to prosessId,
-//                            "opprettet_av" to saksbehandler.navIdent
-//                        )
-//                    ).asUpdate
-//                )
-//            }
-//        }
+        using(sessionOf(dataSource)) { session ->
+            session.transaction { tx ->
+                tx.run(
+                    queryOf(
+                        """
+                            INSERT INTO ia_sak_plan (
+                                plan_id,
+                                ia_prosess,
+                                opprettet_av
+                            )
+                            VALUES (
+                                :plan_id,
+                                :ia_prosess,
+                                :opprettet_av
+                            )
+                        """.trimMargin(),
+                        mapOf(
+                            "plan_id" to planId.toString(),
+                            "ia_prosess" to prosessId,
+                            "opprettet_av" to saksbehandler.navIdent,
+                        ),
+                    ).asUpdate,
+                )
+            }
+            mal.forEach { (temanavn, undertema) ->
+                session.transaction { tx ->
+                    val temaId = tx.run(
+                        queryOf(
+                            """
+                            INSERT INTO ia_sak_plan_tema (
+                                navn,
+                                planlagt,
+                                plan_id
+                            )
+                            VALUES (
+                                :navn,
+                                :planlagt,
+                                :plan_id
+                            )
+                            RETURNING *
+                            """.trimMargin(),
+                            mapOf(
+                                "navn" to temanavn,
+                                "planlagt" to false,
+                                "plan_id" to planId.toString(),
+                            ),
+                        ).map { row: Row ->
+                            row.int("tema_id")
+                        }.asSingle,
+                    )!!
+
+                    undertema.forEach { undertemanavn ->
+                        tx.run(
+                            queryOf(
+                                """
+                            INSERT INTO ia_sak_plan_undertema (
+                                navn,
+                                planlagt,
+                                status,
+                                start_dato, 
+                                slutt_dato,
+                                tema_id,
+                                plan_id
+                                
+                            )
+                            VALUES (
+                                :navn,
+                                :planlagt,
+                                :status,
+                                :start_dato, 
+                                :slutt_dato,
+                                :tema_id,
+                                :plan_id
+                            )
+                                """.trimMargin(),
+                                mapOf(
+                                    "navn" to undertemanavn,
+                                    "planlagt" to false,
+                                    "status" to null,
+                                    "start_dato" to null,
+                                    "slutt_dato" to null,
+                                    "tema_id" to temaId,
+                                    "plan_id" to planId.toString(),
+                                ),
+                            ).asUpdate,
+                        )
+                    }
+                }
+            }
+        }
 
         return hentPlan(prosessId = prosessId)?.right()
             ?: Feil(
@@ -55,52 +154,239 @@ class PlanRepository(
             ).left()
     }
 
-    fun hentPlan(prosessId: Int): Plan? {
-//        TODO: Les plan fra database
-//        using(sessionOf(dataSource)) { session ->
-//            session.run(
-//                queryOf(
-//                    """
-//                        SELECT *
-//                        FROM ia_sak_plan
-//                        WHERE prosess_id = :prosessId
-//                    """.trimMargin(),
-//                    mapOf(
-//                        "prosessId" to prosessId,
-//                    )
-//                ).map { row ->
-//                    Plan(
-//                        id = UUID.fromString(row.string("plan_id")),
-//                        temaer = emptyList(),
-//                        publisert = row.boolean("publisert"),
-//                        sistEndret = row.localDate("sist_endret").toKotlinLocalDate(),
-//                        sistPublisert = row.localDate("sist_publisert").toKotlinLocalDate(),
-//                    )
-//                }.asSingle
-//            )
-//        }
+    fun hentPlan(prosessId: Int): Plan? =
+        using(sessionOf(dataSource)) { session ->
+            session.run(
+                queryOf(
+                    """
+                        SELECT *
+                        FROM ia_sak_plan
+                        WHERE ia_prosess = :prosessId
+                    """.trimMargin(),
+                    mapOf(
+                        "prosessId" to prosessId,
+                    ),
+                ).map { row: Row ->
+                    val planId = UUID.fromString(row.string("plan_id"))
+                    Plan(
+                        id = planId,
+                        sistEndret = row.localDate("sist_endret").toKotlinLocalDate(),
+                        sistPublisert = row.localDateOrNull("sist_publisert")?.toKotlinLocalDate(),
+                        temaer = hentTema(planId, session),
+                    )
+                }.asSingle,
+            )
+        }
 
-        return getHardkodetPlan()
-    }
+    private fun hentTema(
+        planId: UUID,
+        session: Session,
+    ): List<PlanTema> =
+        session.run(
+            queryOf(
+                """
+                        SELECT *
+                        FROM ia_sak_plan_tema
+                        WHERE plan_id = :planId
+                """.trimMargin(),
+                mapOf(
+                    "planId" to planId.toString(),
+                ),
+            ).map { row: Row ->
+                val temaId = row.int("tema_id")
+                PlanTema(
+                    id = temaId,
+                    navn = row.string("navn"),
+                    planlagt = row.boolean("planlagt"),
+                    undertemaer = hentUndertema(planId, temaId, session),
+                    ressurser = hentRessurser(planId, temaId, session),
+                )
+            }.asList,
+        )
+
+    private fun hentTema(
+        planId: UUID,
+        temaId: Int,
+    ): PlanTema? =
+        using(sessionOf(dataSource)) { session ->
+            session.run(
+                queryOf(
+                    """
+                        SELECT *
+                        FROM ia_sak_plan_tema
+                        WHERE plan_id = :planId
+                        AND tema_id = :temaId
+                    """.trimMargin(),
+                    mapOf(
+                        "planId" to planId.toString(),
+                        "temaId" to temaId,
+                    ),
+                ).map { row: Row ->
+                    PlanTema(
+                        id = temaId,
+                        navn = row.string("navn"),
+                        planlagt = row.boolean("planlagt"),
+                        undertemaer = hentUndertema(planId, temaId, session),
+                        ressurser = hentRessurser(planId, temaId, session),
+                    )
+                }.asSingle,
+            )
+        }
+
+    private fun hentUndertema(
+        planId: UUID,
+        temaId: Int,
+        session: Session,
+    ): List<PlanUndertema> =
+        session.run(
+            queryOf(
+                """
+                        SELECT *
+                        FROM ia_sak_plan_undertema
+                        WHERE plan_id = :planId
+                        AND tema_id = :temaId
+                """.trimMargin(),
+                mapOf(
+                    "planId" to planId.toString(),
+                    "temaId" to temaId,
+                ),
+            ).map { row: Row ->
+                PlanUndertema(
+                    id = row.int("undertema_id"),
+                    navn = row.string("navn"),
+                    målsetning = DUMMY_MÅLSETNING,
+                    beskrivelse = DUMMY_BESKRIVELSE,
+                    planlagt = row.boolean("planlagt"),
+                    status = row.stringOrNull("status")?.let { PlanUndertema.Status.valueOf(it) },
+                    startDato = row.localDateOrNull("start_dato")?.toKotlinLocalDate(),
+                    sluttDato = row.localDateOrNull("slutt_dato")?.toKotlinLocalDate(),
+                )
+            }.asList,
+        )
+
+    private fun hentRessurser(
+        planId: UUID,
+        temaId: Int,
+        session: Session,
+    ): List<PlanRessurs> =
+        session.run(
+            queryOf(
+                """
+                        SELECT *
+                        FROM ia_sak_plan_ressurs
+                        WHERE plan_id = :planId
+                        AND tema_id = :temaId
+                """.trimMargin(),
+                mapOf(
+                    "planId" to planId.toString(),
+                    "temaId" to temaId,
+                ),
+            ).map { row: Row ->
+                PlanRessurs(
+                    id = row.int("ressurs_id"),
+                    beskrivelse = row.string("beskrivelse"),
+                    url = row.stringOrNull("url"),
+                )
+            }.asList,
+        )
 
     fun oppdaterTema(
+        planId: UUID,
         temaId: Int,
         undertemaer: List<PlanUndertema>,
-    ): PlanTema {
-        // TODO: oppdater tema i database
-        return getHardkodetPlan().temaer.first { it.id == temaId }.copy(
-            undertemaer = undertemaer,
-        )
+        planlagt: Boolean,
+    ): PlanTema? {
+        using(sessionOf(dataSource)) { session ->
+
+            session.transaction { tx ->
+                tx.run(
+                    queryOf(
+                        """
+                        UPDATE ia_sak_plan_tema SET 
+                            planlagt = :planlagt
+                        WHERE plan_id = :planId
+                        AND tema_id = :temaId
+                        """.trimMargin(),
+                        mapOf(
+                            "planId" to planId.toString(),
+                            "temaId" to temaId,
+                            "planlagt" to planlagt,
+                        ),
+                    ).asUpdate,
+                )
+            }
+
+            undertemaer.forEach { undertema ->
+                oppdaterUndertema(planId = planId, temaId = temaId, endretUndertema = undertema, session = session)
+            }
+        }
+        return hentTema(planId = planId, temaId = temaId)
+    }
+
+    private fun oppdaterUndertema(
+        planId: UUID,
+        temaId: Int,
+        endretUndertema: PlanUndertema,
+        session: Session,
+    ) {
+        session.transaction { tx ->
+            tx.run(
+                queryOf(
+                    """
+                        UPDATE ia_sak_plan_undertema SET 
+                            planlagt = :planlagt,
+                            status = :status,
+                            start_dato = :startDato,
+                            slutt_dato = :sluttDato
+                        WHERE plan_id = :planId
+                        AND tema_id = :temaId
+                        AND undertema_id = :undertemaId
+                    """.trimMargin(),
+                    mapOf(
+                        "planId" to planId.toString(),
+                        "temaId" to temaId,
+                        "undertemaId" to endretUndertema.id,
+                        "planlagt" to endretUndertema.planlagt,
+                        "status" to endretUndertema.status?.name,
+                        "startDato" to endretUndertema.startDato,
+                        "sluttDato" to endretUndertema.sluttDato,
+                    ),
+                ).asUpdate,
+            )
+        }
     }
 
     fun oppdaterUndertema(
+        planId: UUID,
         temaId: Int,
-        undertemaId: Int,
-        endretUndertema: PlanUndertema,
-    ): PlanUndertema {
-        // TODO: oppdater undertema i database
-        return getHardkodetPlan().temaer.first { it.id == temaId }.undertemaer.first { it.id == undertemaId }.copy(
-            status = endretUndertema.status,
-        )
+        undertema: PlanUndertema,
+    ) = using(sessionOf(dataSource)) { session ->
+        session.transaction { tx ->
+            tx.run(
+                queryOf(
+                    """
+                    UPDATE ia_sak_plan_undertema SET 
+                        planlagt = :planlagt,
+                        status = :status,
+                        start_dato = :startDato,
+                        slutt_dato = :sluttDato
+                    WHERE plan_id = :planId
+                    AND tema_id = :temaId
+                    AND undertema_id = :undertemaId
+                    """.trimMargin(),
+                    mapOf(
+                        "planId" to planId.toString(),
+                        "temaId" to temaId,
+                        "undertemaId" to undertema.id,
+                        "planlagt" to undertema.planlagt,
+                        "status" to undertema.status?.name,
+                        "startDato" to undertema.startDato,
+                        "sluttDato" to undertema.sluttDato,
+                    ),
+                ).asUpdate,
+            )
+        }
+
+        hentUndertema(planId = planId, temaId = temaId, session = session).firstOrNull { it.id == undertema.id }
     }
 }
--- src/test/kotlin/no/nav/lydia/container/ia/sak/plan/PlanApiTest.kt
+++ src/test/kotlin/no/nav/lydia/container/ia/sak/plan/PlanApiTest.kt
@@ -1,9 +1,12 @@
 package no.nav.lydia.container.ia.sak.plan
 
+import io.kotest.matchers.collections.shouldHaveSize
 import io.kotest.matchers.shouldBe
+import no.nav.lydia.helper.IASakKartleggingHelper.Companion.opprettKartlegging
 import no.nav.lydia.helper.PlanHelper
 import no.nav.lydia.helper.PlanHelper.Companion.tilRequest
 import no.nav.lydia.helper.SakHelper.Companion.nySakIKartlegges
+import no.nav.lydia.helper.hentIAProsesser
 import no.nav.lydia.helper.tilSingelRespons
 import no.nav.lydia.ia.sak.api.plan.PlanDto
 import no.nav.lydia.ia.sak.domene.plan.PlanUndertema
@@ -13,6 +16,9 @@ class PlanApiTest {
     @Test
     fun `oppretter en ny plan`() {
         val sak = nySakIKartlegges()
+        sak.opprettKartlegging()
+        val prosesser = sak.hentIAProsesser()
+        prosesser shouldHaveSize 1
 
         val resp =
             PlanHelper.opprettPlan(orgnr = sak.orgnr, saksnummer = sak.saksnummer)
@@ -30,29 +36,57 @@ class PlanApiTest {
     @Test
     fun `kan endre tema`() {
         val sak = nySakIKartlegges()
+        sak.opprettKartlegging()
+        val prosesser = sak.hentIAProsesser()
+        prosesser shouldHaveSize 1
+        val opprettetPlan = PlanHelper.opprettPlan(orgnr = sak.orgnr, saksnummer = sak.saksnummer)
+            .tilSingelRespons<PlanDto>()
+        opprettetPlan.third.get().temaer.size shouldBe 4
 
-        val plan = PlanHelper.hentPlan(orgnr = sak.orgnr, saksnummer = sak.saksnummer)
+        val lagretPlan = PlanHelper.hentPlan(orgnr = sak.orgnr, saksnummer = sak.saksnummer)
+
+        val endretPlan = lagretPlan.copy(
+            temaer = lagretPlan.temaer.map { temaDto ->
+                temaDto.copy(
+                    undertemaer = temaDto.undertemaer.map { undertemaDto ->
+                        undertemaDto.copy(
+                            planlagt = true,
+                        )
+                    },
+                )
+            },
+        )
 
         val resp =
             PlanHelper.endreTema(
                 orgnr = sak.orgnr,
                 saksnummer = sak.saksnummer,
-                temaId = plan.temaer.first().id,
-                endring = plan.tilRequest().first().undertemaer,
+                temaId = endretPlan.temaer.first().id,
+                endring = endretPlan.tilRequest().first().undertemaer,
             )
 
-        // TODO: Sjekk i database at det ble lagret rett
-//        postgresContainer
-//            .hentEnkelKolonne<String>(
-//                "select kartlegging_id from ia_sak_kartlegging where kartlegging_id = '${resp.third.get().kartleggingId}'",
-//            ) shouldNotBe null
+        resp.id shouldBe endretPlan.temaer.first().id
+        resp.navn shouldBe endretPlan.temaer.first().navn
+        resp.undertemaer.first().id shouldBe endretPlan.temaer.first().undertemaer.first().id
+        resp.undertemaer.first().planlagt shouldBe endretPlan.temaer.first().undertemaer.first().planlagt
+        resp.undertemaer.first().navn shouldBe endretPlan.temaer.first().undertemaer.first().navn
+        resp.undertemaer.first().beskrivelse shouldBe endretPlan.temaer.first().undertemaer.first().beskrivelse
+        resp.undertemaer.first().målsetning shouldBe endretPlan.temaer.first().undertemaer.first().målsetning
+        resp.undertemaer.first().status shouldBe PlanUndertema.Status.PLANLAGT
     }
 
     @Test
     fun `kan endre status på undertema`() {
         val sak = nySakIKartlegges()
+        sak.opprettKartlegging()
+        val prosesser = sak.hentIAProsesser()
+        prosesser shouldHaveSize 1
+        val opprettetPlan = PlanHelper.opprettPlan(orgnr = sak.orgnr, saksnummer = sak.saksnummer)
+            .tilSingelRespons<PlanDto>()
+        opprettetPlan.third.get().temaer.size shouldBe 4
+        val plan = PlanHelper.hentPlan(orgnr = sak.orgnr, saksnummer = sak.saksnummer)
+        val førsteTema = plan.temaer.first()
 
-        val førsteTema = PlanHelper.hentPlan(orgnr = sak.orgnr, saksnummer = sak.saksnummer).temaer.first()
         val førsteUndertema = førsteTema.undertemaer.first()
 
         val nyStatus = PlanUndertema.Status.FULLFØRT
@@ -67,26 +101,43 @@ class PlanApiTest {
             )
 
         resp.status shouldBe nyStatus
-
-        // TODO: Sjekk i database at det ble lagret rett
-//        postgresContainer
-//            .hentEnkelKolonne<String>(
-//                "select kartlegging_id from ia_sak_kartlegging where kartlegging_id = '${resp.third.get().kartleggingId}'",
-//            ) shouldNotBe null
     }
 
     @Test
     fun `kan endre flere tema`() {
         val sak = nySakIKartlegges()
+        sak.opprettKartlegging()
+        val prosesser = sak.hentIAProsesser()
+        prosesser shouldHaveSize 1
+        val opprettetPlan = PlanHelper.opprettPlan(orgnr = sak.orgnr, saksnummer = sak.saksnummer)
+            .tilSingelRespons<PlanDto>()
+        opprettetPlan.third.get().temaer.size shouldBe 4
 
-        val plan = PlanHelper.hentPlan(orgnr = sak.orgnr, saksnummer = sak.saksnummer)
+        val lagretPlan = PlanHelper.hentPlan(orgnr = sak.orgnr, saksnummer = sak.saksnummer)
 
-        val resp = PlanHelper.endrePlan(orgnr = sak.orgnr, saksnummer = sak.saksnummer, endring = plan.tilRequest())
+        val endretPlan = lagretPlan.copy(
+            temaer = lagretPlan.temaer.map { temaDto ->
+                temaDto.copy(
+                    planlagt = true,
+                    undertemaer = temaDto.undertemaer.map { undertemaDto ->
+                        undertemaDto.copy(
+                            planlagt = true,
+                        )
+                    },
+                )
+            },
+        )
 
-        // TODO: Sjekk i database at det ble lagret rett
-//        postgresContainer
-//            .hentEnkelKolonne<String>(
-//                "select kartlegging_id from ia_sak_kartlegging where kartlegging_id = '${resp.third.get().kartleggingId}'",
-//            ) shouldNotBe null
+        val resp = PlanHelper.endrePlan(orgnr = sak.orgnr, saksnummer = sak.saksnummer, endring = endretPlan.tilRequest())
+
+        resp.first().id shouldBe endretPlan.temaer.first().id
+        resp.first().navn shouldBe endretPlan.temaer.first().navn
+        resp.first().planlagt shouldBe endretPlan.temaer.first().planlagt
+        resp.first().undertemaer.first().id shouldBe endretPlan.temaer.first().undertemaer.first().id
+        resp.first().undertemaer.first().planlagt shouldBe endretPlan.temaer.first().undertemaer.first().planlagt
+        resp.first().undertemaer.first().navn shouldBe endretPlan.temaer.first().undertemaer.first().navn
+        resp.first().undertemaer.first().beskrivelse shouldBe endretPlan.temaer.first().undertemaer.first().beskrivelse
+        resp.first().undertemaer.first().målsetning shouldBe endretPlan.temaer.first().undertemaer.first().målsetning
+        resp.first().undertemaer.first().status shouldBe PlanUndertema.Status.PLANLAGT
     }
 }
