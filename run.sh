450ea0eb4ac57fb59a15fc447fd548c09a87cc8b
Sindre Larsen <silar.dev@proton.me>
2024-10-24 16:40:30 +0200

    oppdaterer db script
    Co-authored-by: Thomas Dufourd <thomas.dufourd@nav.no>
    Co-authored-by: Nima Jimale <nima.jimale@nav.no>

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 ./gradlew build -x test
 docker-compose up -d
-curl -o db_script.sql https://raw.githubusercontent.com/navikt/lydia-api/main/scripts/db/lydia-api-container-db_localhost-2024_10_24_15_46_31-dump.sql
+curl -o db_script.sql https://raw.githubusercontent.com/navikt/lydia-api/main/scripts/db/lydia-api-container-db_localhost-2024_10_24_16_39_14-dump.sql
 sleep 3
 PGPASSWORD=test psql -h localhost -p 5432 -U postgres -f db_script.sql > /dev/null
 rm db_script.sql
