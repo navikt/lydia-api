4e3f2791006b190d660b3088d62d0d656066b0e1
Sindre Larsen <silar.dev@proton.me>
2024-10-24 16:38:00 +0200

    Legg til grunnleggende spørsmål for evaluering
    Co-authored-by: Thomas Dufourd <thomas.dufourd@nav.no>
    Co-authored-by: Nima Jimale <nima.jimale@nav.no>

---
+++ src/main/resources/db/migration/2024/V94__forste_batteri_med_sporsmal_og_svar_for_evaluering.sql
@@ -0,0 +1,149 @@
+-- spm 1 Partssamarbeid
+insert into ia_sak_kartlegging_sporsmal (sporsmal_id, sporsmal_tekst)
+VALUES ('0192bedd-7815-7abf-ae8d-f04f4aed97df',
+        'Vi har konkretiserte planer for hvordan vi i partssamarbeidet skal samarbeide fremover');
+
+insert into ia_sak_kartlegging_svaralternativer (svaralternativ_id, sporsmal_id, svaralternativ_tekst)
+VALUES (gen_random_uuid(), '0192bedd-7815-7abf-ae8d-f04f4aed97df', 'Enig');
+insert into ia_sak_kartlegging_svaralternativer (svaralternativ_id, sporsmal_id, svaralternativ_tekst)
+VALUES (gen_random_uuid(), '0192bedd-7815-7abf-ae8d-f04f4aed97df', 'Litt enig');
+insert into ia_sak_kartlegging_svaralternativer (svaralternativ_id, sporsmal_id, svaralternativ_tekst)
+VALUES (gen_random_uuid(), '0192bedd-7815-7abf-ae8d-f04f4aed97df', 'Hverken enig eller uenig');
+insert into ia_sak_kartlegging_svaralternativer (svaralternativ_id, sporsmal_id, svaralternativ_tekst)
+VALUES (gen_random_uuid(), '0192bedd-7815-7abf-ae8d-f04f4aed97df', 'Litt uenig');
+insert into ia_sak_kartlegging_svaralternativer (svaralternativ_id, sporsmal_id, svaralternativ_tekst)
+VALUES (gen_random_uuid(), '0192bedd-7815-7abf-ae8d-f04f4aed97df', 'Uenig');
+
+-- spm 2 Partssamarbeid
+insert into ia_sak_kartlegging_sporsmal (sporsmal_id, sporsmal_tekst)
+VALUES ('0192bee0-6437-7302-a16e-79a1e9e62d5b',
+        'Jeg opplever at vi er motiverte for å samarbeide om å forebygge sykefravær og utvikle arbeidsmiljøet');
+
+insert into ia_sak_kartlegging_svaralternativer (svaralternativ_id, sporsmal_id, svaralternativ_tekst)
+VALUES (gen_random_uuid(), '0192bee0-6437-7302-a16e-79a1e9e62d5b', 'Enig');
+insert into ia_sak_kartlegging_svaralternativer (svaralternativ_id, sporsmal_id, svaralternativ_tekst)
+VALUES (gen_random_uuid(), '0192bee0-6437-7302-a16e-79a1e9e62d5b', 'Litt enig');
+insert into ia_sak_kartlegging_svaralternativer (svaralternativ_id, sporsmal_id, svaralternativ_tekst)
+VALUES (gen_random_uuid(), '0192bee0-6437-7302-a16e-79a1e9e62d5b', 'Hverken enig eller uenig');
+insert into ia_sak_kartlegging_svaralternativer (svaralternativ_id, sporsmal_id, svaralternativ_tekst)
+VALUES (gen_random_uuid(), '0192bee0-6437-7302-a16e-79a1e9e62d5b', 'Litt uenig');
+insert into ia_sak_kartlegging_svaralternativer (svaralternativ_id, sporsmal_id, svaralternativ_tekst)
+VALUES (gen_random_uuid(), '0192bee0-6437-7302-a16e-79a1e9e62d5b', 'Uenig');
+
+-- spm 3 Partssamarbeid
+insert into ia_sak_kartlegging_sporsmal (sporsmal_id, sporsmal_tekst)
+VALUES ('0192bee1-c50d-76b8-850f-100d810ad6c3',
+        'Jeg opplever at vi har nødvendige verktøy for å samarbeide om å forebygge sykefravær og utvikle arbeidsmiljøet');
+
+insert into ia_sak_kartlegging_svaralternativer (svaralternativ_id, sporsmal_id, svaralternativ_tekst)
+VALUES (gen_random_uuid(), '0192bee1-c50d-76b8-850f-100d810ad6c3', 'Enig');
+insert into ia_sak_kartlegging_svaralternativer (svaralternativ_id, sporsmal_id, svaralternativ_tekst)
+VALUES (gen_random_uuid(), '0192bee1-c50d-76b8-850f-100d810ad6c3', 'Litt enig');
+insert into ia_sak_kartlegging_svaralternativer (svaralternativ_id, sporsmal_id, svaralternativ_tekst)
+VALUES (gen_random_uuid(), '0192bee1-c50d-76b8-850f-100d810ad6c3', 'Hverken enig eller uenig');
+insert into ia_sak_kartlegging_svaralternativer (svaralternativ_id, sporsmal_id, svaralternativ_tekst)
+VALUES (gen_random_uuid(), '0192bee1-c50d-76b8-850f-100d810ad6c3', 'Litt uenig');
+insert into ia_sak_kartlegging_svaralternativer (svaralternativ_id, sporsmal_id, svaralternativ_tekst)
+VALUES (gen_random_uuid(), '0192bee1-c50d-76b8-850f-100d810ad6c3', 'Uenig');
+
+
+-- spm 1 Sykefraværsarbeid
+insert into ia_sak_kartlegging_sporsmal (sporsmal_id, sporsmal_tekst)
+VALUES ('0192bee3-ea9e-7366-877e-c52e03060d40',
+        'Opplever du at samarbeidet om forebyggende sykefraværsarbeid bidrar til redusert sykefravær?');
+
+insert into ia_sak_kartlegging_svaralternativer (svaralternativ_id, sporsmal_id, svaralternativ_tekst)
+VALUES (gen_random_uuid(), '0192bee3-ea9e-7366-877e-c52e03060d40', 'Enig');
+insert into ia_sak_kartlegging_svaralternativer (svaralternativ_id, sporsmal_id, svaralternativ_tekst)
+VALUES (gen_random_uuid(), '0192bee3-ea9e-7366-877e-c52e03060d40', 'Litt enig');
+insert into ia_sak_kartlegging_svaralternativer (svaralternativ_id, sporsmal_id, svaralternativ_tekst)
+VALUES (gen_random_uuid(), '0192bee3-ea9e-7366-877e-c52e03060d40', 'Hverken enig eller uenig');
+insert into ia_sak_kartlegging_svaralternativer (svaralternativ_id, sporsmal_id, svaralternativ_tekst)
+VALUES (gen_random_uuid(), '0192bee3-ea9e-7366-877e-c52e03060d40', 'Litt uenig');
+insert into ia_sak_kartlegging_svaralternativer (svaralternativ_id, sporsmal_id, svaralternativ_tekst)
+VALUES (gen_random_uuid(), '0192bee3-ea9e-7366-877e-c52e03060d40', 'Uenig');
+
+
+-- spm 1 Arbeidsmiljø
+insert into ia_sak_kartlegging_sporsmal (sporsmal_id, sporsmal_tekst)
+VALUES ('0192bee5-0bf8-74a9-9569-ee9118d4bdb6',
+        'Vi er motiverte for å videreutvikle arbeidsmiljøet vårt');
+
+insert into ia_sak_kartlegging_svaralternativer (svaralternativ_id, sporsmal_id, svaralternativ_tekst)
+VALUES (gen_random_uuid(), '0192bee5-0bf8-74a9-9569-ee9118d4bdb6', 'Enig');
+insert into ia_sak_kartlegging_svaralternativer (svaralternativ_id, sporsmal_id, svaralternativ_tekst)
+VALUES (gen_random_uuid(), '0192bee5-0bf8-74a9-9569-ee9118d4bdb6', 'Litt enig');
+insert into ia_sak_kartlegging_svaralternativer (svaralternativ_id, sporsmal_id, svaralternativ_tekst)
+VALUES (gen_random_uuid(), '0192bee5-0bf8-74a9-9569-ee9118d4bdb6', 'Hverken enig eller uenig');
+insert into ia_sak_kartlegging_svaralternativer (svaralternativ_id, sporsmal_id, svaralternativ_tekst)
+VALUES (gen_random_uuid(), '0192bee5-0bf8-74a9-9569-ee9118d4bdb6', 'Litt uenig');
+insert into ia_sak_kartlegging_svaralternativer (svaralternativ_id, sporsmal_id, svaralternativ_tekst)
+VALUES (gen_random_uuid(), '0192bee5-0bf8-74a9-9569-ee9118d4bdb6', 'Uenig');
+
+-- spm 2 Arbeidsmiljø
+insert into ia_sak_kartlegging_sporsmal (sporsmal_id, sporsmal_tekst)
+VALUES ('0192bee6-55aa-7281-b18d-d519c9d0d86c',
+        'Vi har fått et godt grunnlag for å videreutvikle arbeidsmiljøet vårt');
+
+insert into ia_sak_kartlegging_svaralternativer (svaralternativ_id, sporsmal_id, svaralternativ_tekst)
+VALUES (gen_random_uuid(), '0192bee6-55aa-7281-b18d-d519c9d0d86c', 'Enig');
+insert into ia_sak_kartlegging_svaralternativer (svaralternativ_id, sporsmal_id, svaralternativ_tekst)
+VALUES (gen_random_uuid(), '0192bee6-55aa-7281-b18d-d519c9d0d86c', 'Litt enig');
+insert into ia_sak_kartlegging_svaralternativer (svaralternativ_id, sporsmal_id, svaralternativ_tekst)
+VALUES (gen_random_uuid(), '0192bee6-55aa-7281-b18d-d519c9d0d86c', 'Hverken enig eller uenig');
+insert into ia_sak_kartlegging_svaralternativer (svaralternativ_id, sporsmal_id, svaralternativ_tekst)
+VALUES (gen_random_uuid(), '0192bee6-55aa-7281-b18d-d519c9d0d86c', 'Litt uenig');
+insert into ia_sak_kartlegging_svaralternativer (svaralternativ_id, sporsmal_id, svaralternativ_tekst)
+VALUES (gen_random_uuid(), '0192bee6-55aa-7281-b18d-d519c9d0d86c', 'Uenig');
+
+-- spm 3 Arbeidsmiljø
+insert into ia_sak_kartlegging_sporsmal (sporsmal_id, sporsmal_tekst)
+VALUES ('0192bee7-557b-7611-9c2a-6fb8c72691bc',
+        'Jeg tror det forebyggende arbeidet vil gi oss et bedre et arbeidsmiljø');
+
+insert into ia_sak_kartlegging_svaralternativer (svaralternativ_id, sporsmal_id, svaralternativ_tekst)
+VALUES (gen_random_uuid(), '0192bee7-557b-7611-9c2a-6fb8c72691bc', 'Enig');
+insert into ia_sak_kartlegging_svaralternativer (svaralternativ_id, sporsmal_id, svaralternativ_tekst)
+VALUES (gen_random_uuid(), '0192bee7-557b-7611-9c2a-6fb8c72691bc', 'Litt enig');
+insert into ia_sak_kartlegging_svaralternativer (svaralternativ_id, sporsmal_id, svaralternativ_tekst)
+VALUES (gen_random_uuid(), '0192bee7-557b-7611-9c2a-6fb8c72691bc', 'Hverken enig eller uenig');
+insert into ia_sak_kartlegging_svaralternativer (svaralternativ_id, sporsmal_id, svaralternativ_tekst)
+VALUES (gen_random_uuid(), '0192bee7-557b-7611-9c2a-6fb8c72691bc', 'Litt uenig');
+insert into ia_sak_kartlegging_svaralternativer (svaralternativ_id, sporsmal_id, svaralternativ_tekst)
+VALUES (gen_random_uuid(), '0192bee7-557b-7611-9c2a-6fb8c72691bc', 'Uenig');
+
+-- Første tema for evaluering
+INSERT INTO ia_sak_kartlegging_tema (tema_id, navn, beskrivelse, introtekst, rekkefolge, status, type)
+VALUES (7, '','Partssamarbeid','', 1, 'AKTIV', 'Evaluering');
+
+-- Legg til alle 3 spørsmål i tema Partssamarbeid
+INSERT INTO ia_sak_kartlegging_tema_til_spørsmål (tema_id, sporsmal_id)
+VALUES (7, '0192bedd-7815-7abf-ae8d-f04f4aed97df');
+
+INSERT INTO ia_sak_kartlegging_tema_til_spørsmål (tema_id, sporsmal_id)
+VALUES (7, '0192bee0-6437-7302-a16e-79a1e9e62d5b');
+
+INSERT INTO ia_sak_kartlegging_tema_til_spørsmål (tema_id, sporsmal_id)
+VALUES (7, '0192bee1-c50d-76b8-850f-100d810ad6c3');
+
+-- Andre tema for evaluering
+INSERT INTO ia_sak_kartlegging_tema (tema_id, navn, beskrivelse, introtekst, rekkefolge, status, type)
+VALUES (8, '', 'Sykefraværsarbeid', '', 2, 'AKTIV', 'Evaluering');
+
+-- Legg til 1 spørsmål i tema Sykefraværsarbeid
+INSERT INTO ia_sak_kartlegging_tema_til_spørsmål (tema_id, sporsmal_id)
+VALUES (8, '0192bee3-ea9e-7366-877e-c52e03060d40');
+
+-- Tredje tema for evaluering
+INSERT INTO ia_sak_kartlegging_tema (tema_id, navn, beskrivelse, introtekst, rekkefolge, status, type)
+VALUES (9, '', 'Arbeidsmiljø', '', 3, 'AKTIV', 'Evaluering');
+
+-- Legg til alle 3 spørsmål i tema Arbeidsmiljø
+INSERT INTO ia_sak_kartlegging_tema_til_spørsmål (tema_id, sporsmal_id)
+VALUES (9, '0192bee5-0bf8-74a9-9569-ee9118d4bdb6');
+
+INSERT INTO ia_sak_kartlegging_tema_til_spørsmål (tema_id, sporsmal_id)
+VALUES (9, '0192bee6-55aa-7281-b18d-d519c9d0d86c');
+
+INSERT INTO ia_sak_kartlegging_tema_til_spørsmål (tema_id, sporsmal_id)
+VALUES (9, '0192bee7-557b-7611-9c2a-6fb8c72691bc');
