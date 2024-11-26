ac2c5493ef53607b1d92eede292839f31a7c1ed3
Sindre Larsen <silar.dev@proton.me>
2024-11-26 13:48:48 +0100

    flytter alle python scripts til pia-hub

--- scripts/src/dataklasser/Svaralternativ.py
+++
@@ -1,7 +0,0 @@
-from util.generer_uuider import generer_uuid_med_delay
-
-
-class Svaralternativ:
-    def __init__(self, tekst: str):
-        self.id = generer_uuid_med_delay()
-        self.tekst = tekst
