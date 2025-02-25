62ae6566092707f98d3b523c3bea9fb5d8e443d7
nimajimale <nima0512@gmail.com>
2025-02-25 13:26:10 +0100

    Default til null for optional felter
    Co-authored-by: Christian Klem <christian.klem@nav.no>

--- src/test/kotlin/no/nav/lydia/container/integrasjoner/salesforce/SalesforceAktivitetKonsumentTest.kt
+++ src/test/kotlin/no/nav/lydia/container/integrasjoner/salesforce/SalesforceAktivitetKonsumentTest.kt
@@ -26,7 +26,18 @@ class SalesforceAktivitetKonsumentTest {
             Topic.SALESFORCE_AKTIVITET_TOPIC,
         )
 
-        TestContainerHelper.lydiaApiContainer shouldNotContainLog "Hentet aktivitet. id: 'id2', type: 'oppgave'".toRegex()
+        TestContainerHelper.lydiaApiContainer shouldNotContainLog "Hentet aktivitet. id: 'id2', type: 'Oppgave'".toRegex()
+    }
+
+    @Test
+    fun `skal konsumere meldinger med manglende felt`() {
+        TestContainerHelper.kafkaContainerHelper.sendOgVentTilKonsumert(
+            "nøkkel",
+            meldingMedManglendeFelt(id = "id3"),
+            Topic.SALESFORCE_AKTIVITET_TOPIC,
+        )
+
+        TestContainerHelper.lydiaApiContainer shouldContainLog "Hentet aktivitet. id: 'id3', type: 'Oppgave', saksnummer: 'saksnummer'".toRegex()
     }
 
     private fun melding(
@@ -47,7 +58,7 @@ class SalesforceAktivitetKonsumentTest {
           "ActivityType__c": "Prioritert IA (Fia)",
           "DurationInMinutes__c": null,
           "EndDateTime__c": null,
-          "IACaseNumber__c": "$saksnummer",
+          "IACaseNumber__c": ${nullOrStringWithQuotes(saksnummer)},
           "IACooperationId__c": "",
           "IASubtheme__c": "Sykefraværsrutiner",
           "Id__c": "$id",
@@ -74,4 +85,51 @@ class SalesforceAktivitetKonsumentTest {
           "OwnerId__c": "00xxx"
         }
         """.trimIndent()
+
+    private fun meldingMedManglendeFelt(
+        id: String,
+        saksnummer: String? = "saksnummer",
+    ) = """
+        {
+          "CreatedDate": "2025-02-24T18:07:47.467Z",
+          "CreatedById": "createdbyid",
+          "EventObject__c": "Task",
+          "EventType__c": "Created",
+          "AccountNavUnit__c": "",
+          "AccountOrgNumber__c": "123456789",
+          "AccountOrgType__c": "",
+          "AccountParentId__c": "",
+          "AccountParentOrgNumber__c": "987654321",
+          "ActivityDate__c": "2025-02-26T00:00:00Z",
+          "ActivityType__c": "Prioritert IA (Fia)",
+          "DurationInMinutes__c": null,
+          "EndDateTime__c": null,
+          "IACaseNumber__c": ${nullOrStringWithQuotes(saksnummer)},
+          "IACooperationId__c": "",
+          "IASubtheme__c": "Sykefraværsrutiner",
+          "Id__c": "$id",
+          "LastModifiedDate__c": "2025-02-24T18:07:47Z",
+          "Priority__c": "Normal",
+          "RecordTypeId__c": "rectypid",
+          "RecordTypeName__c": "IA_task",
+          "Region__c": "Oslo",
+          "ReminderDateTime__c": "2025-02-26T07:00:00Z",
+          "Service__c": "Sykefraværsarbeid",
+          "StartDateTime__c": null,
+          "Status__c": "Fullført",
+          "Subject__c": "Test for Pia",
+          "TaskEvent__c": "Oppgave",
+          "CompletedDate__c": null,
+          "Type__c": "",
+          "Unit__c": "NAV IT",
+          "UserNavUnit__c": "2940",
+          "WhatId__c": "what",
+          "WhoId__c": "",
+          "DeletedDate__c": null,
+          "LastModifiedById__c": "00xxx",
+          "OwnerId__c": "00xxx"
+        }
+        """.trimIndent()
+
+    private fun nullOrStringWithQuotes(value: String?): String? = if (value != null) """"$value"""" else null
 }
