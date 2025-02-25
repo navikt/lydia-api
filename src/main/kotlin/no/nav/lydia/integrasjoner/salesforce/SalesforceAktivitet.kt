44e994a004e6ad91d7b7a7943cfe778c9798835b
nimajimale <nima0512@gmail.com>
2025-02-25 13:26:10 +0100

    Default til null for optional felter
    Co-authored-by: Christian Klem <christian.klem@nav.no>

--- src/main/kotlin/no/nav/lydia/integrasjoner/salesforce/SalesforceAktivitet.kt
+++ src/main/kotlin/no/nav/lydia/integrasjoner/salesforce/SalesforceAktivitet.kt
@@ -8,18 +8,18 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class SalesforceAktivitet(
     val Id__c: String, // -- Id til aktivitet i SF
-    val ActivityCreatedDate__c: String?, // -- Når aktiviteten ble opprettet i SF
+    val ActivityCreatedDate__c: String? = null, // -- Når aktiviteten ble opprettet i SF
     val EventObject__c: String, // -- Objekttype i SF (Task ; Event)
     val TaskEvent__c: String, // -- Hva slags aktivitet (Møte ; Oppgave)
     val EventType__c: String, // -- Type hendelse (Created ; Updated ; Deleted ; Undeleted)
-    val Type__c: String?, // -- Hvilken kanal er brukt (Call ; Email ; SMS ; Meeting ; Other)
-    val ActivityDate__c: String?, // -- Planlagt tid for aktivitet
-    val Status__c: String?, // -- Status på aktivitet (Åpen ; Fullført)
-//    val Subject__c: String?, // -- Fritekst emne for aktivitet
-    val AccountOrgNumber__c: String?, // -- Orgnummer aktiviteten gjelder
-    val ActivityType__c: String?, // -- Aktivitetstype
-    val IACaseNumber__c: String?, // -- IA saksnummer
-    val IACooperationId__c: String?, // -- Samarbeidsid
-    val Service__c: String?, // -- Plan - Tema
-    val IASubtheme__c: String?, // -- Plan - Undertema
+    val Type__c: String? = null, // -- Hvilken kanal er brukt (Call ; Email ; SMS ; Meeting ; Other)
+    val ActivityDate__c: String? = null, // -- Planlagt tid for aktivitet
+    val Status__c: String? = null, // -- Status på aktivitet (Åpen ; Fullført)
+//    val Subject__c: String? = null, // -- Fritekst emne for aktivitet
+    val AccountOrgNumber__c: String? = null, // -- Orgnummer aktiviteten gjelder
+    val ActivityType__c: String? = null, // -- Aktivitetstype
+    val IACaseNumber__c: String? = null, // -- IA saksnummer
+    val IACooperationId__c: String? = null, // -- Samarbeidsid
+    val Service__c: String? = null, // -- Plan - Tema
+    val IASubtheme__c: String? = null, // -- Plan - Undertema
 )
