354ff0d039d022ae625f99fd244a6f8bf8cf2009
Thomas Dufourd <thomas.dufourd@gmail.com>
2025-05-30 14:28:36 +0200

    Gjør testen mer robust ved å spesifisere et (test) kommunenummer for virksomheten vi tester på, slik at vi henter ønskede saker på virksomhet i prioriteringslista
    Co-authored-by: Nima Jimale <nima.jimale@nav.no>

--- src/test/kotlin/no/nav/lydia/container/sykefraværsstatistikk/UthentingAvPrioriteringslistaTest.kt
+++ src/test/kotlin/no/nav/lydia/container/sykefraværsstatistikk/UthentingAvPrioriteringslistaTest.kt
@@ -2,17 +2,27 @@ package no.nav.lydia.container.sykefraværsstatistikk
 
 import io.kotest.matchers.shouldBe
 import no.nav.lydia.helper.SakHelper.Companion.fullførSak
+import no.nav.lydia.helper.SakHelper.Companion.hentSamarbeidshistorikk
 import no.nav.lydia.helper.SakHelper.Companion.nySakIKontaktes
 import no.nav.lydia.helper.SakHelper.Companion.nySakIViBistår
 import no.nav.lydia.helper.StatistikkHelper.Companion.hentSykefravær
 import no.nav.lydia.helper.TestContainerHelper
+import no.nav.lydia.helper.TestVirksomhet.Companion.beliggenhet
+import no.nav.lydia.helper.TestVirksomhet.Companion.nyVirksomhet
+import no.nav.lydia.helper.VirksomhetHelper.Companion.lastInnNyVirksomhet
 import no.nav.lydia.ia.sak.domene.IAProsessStatus
+import no.nav.lydia.sykefraværsstatistikk.api.geografi.Kommune
 import kotlin.test.Test
 
 class UthentingAvPrioriteringslistaTest {
     @Test
     fun `Status som vises i prioriteringslista, er status på saken som ble opprettet sist i virksomhet`() {
-        val fullførtSak = nySakIViBistår().fullførSak()
+        val testKommune = Kommune(navn = "Den skal være unik", nummer = "1331")
+        val virksomhet =
+            lastInnNyVirksomhet(nyVirksomhet = nyVirksomhet(beliggenhet = beliggenhet(kommune = testKommune)))
+
+        val fullførtSak = nySakIViBistår(orgnummer = virksomhet.orgnr).fullførSak()
+        virksomhet.orgnr shouldBe fullførtSak.orgnr
         nySakIKontaktes(orgnummer = fullførtSak.orgnr)
         // Oppdater 'endret' dato for å simulere 'maskinelt oppdatering' på en eldre sak
         TestContainerHelper.postgresContainerHelper.performUpdate(
@@ -20,8 +30,10 @@ class UthentingAvPrioriteringslistaTest {
                 "where orgnr = '${fullførtSak.orgnr}' " +
                 "and saksnummer = '${fullførtSak.saksnummer}'",
         )
+        val historikk = hentSamarbeidshistorikk(fullførtSak.orgnr)
+        historikk.size shouldBe 2
         // Hent prioriteringslista
-        val virksomheter = hentSykefravær()
+        val virksomheter = hentSykefravær(kommuner = testKommune.nummer)
 
         virksomheter.data.first { it.orgnr == fullførtSak.orgnr }
             .also { virksomhet ->
