35955ada6e3f96c2faba21922a59418ff2bfefb4
Sindre Larsen <silar.dev@proton.me>
2025-07-10 09:33:55 +0200

    deploy til dev

--- src/main/kotlin/no/nav/lydia/ia/sak/db/SpørreundersøkelseRepository.kt
+++ src/main/kotlin/no/nav/lydia/ia/sak/db/SpørreundersøkelseRepository.kt
@@ -31,6 +31,10 @@ import javax.sql.DataSource
 class SpørreundersøkelseRepository(
     private val dataSource: DataSource,
 ) {
+    companion object {
+        const val MINIMUM_ANTALL_SVAR_FØR_MASKERING = Spørreundersøkelse.MINIMUM_ANTALL_DELTAKERE
+    }
+
     fun hentSpørreundersøkelser(
         samarbeid: IASamarbeid,
         type: Spørreundersøkelse.Type,
@@ -74,7 +78,7 @@ class SpørreundersøkelseRepository(
                                            WHEN (SELECT COUNT(DISTINCT us.sesjon_id)
                                                  FROM ia_sak_kartlegging_svar us
                                                  WHERE us.kartlegging_id = sporreundersokelse.kartlegging_id
-                                                   AND us.sporsmal_id = sporsmal.sporsmal_id) < 3
+                                                   AND us.sporsmal_id = sporsmal.sporsmal_id) < $MINIMUM_ANTALL_SVAR_FØR_MASKERING
                                                THEN 0
                                            ELSE COUNT(DISTINCT svar.sesjon_id)
                                            END
@@ -121,83 +125,83 @@ class SpørreundersøkelseRepository(
         ).tilSpørreundersøkelser()
     }
 
-    fun hentSpørreundersøkelse(spørreundersøkelseId: UUID) =
+    fun hentSpørreundersøkelse(spørreundersøkelseId: UUID): Spørreundersøkelse? =
         using(sessionOf(dataSource)) { session ->
             session.run(
                 queryOf(
                     """
-                SELECT virksomhet.navn,
-                       virksomhet.orgnr,
-                       sak.saksnummer,
-                       samarbeid.id                                          AS samarbeid_id,
-                       sporreundersokelse.kartlegging_id                     AS sporreundersokelse_id,
-                       sporreundersokelse.type                               AS type,
-                       sporreundersokelse.status                             AS status,
-                       sporreundersokelse.opprettet_av,
-                       sporreundersokelse.opprettet,
-                       sporreundersokelse.gyldig_til,
-                       sporreundersokelse.endret,
-                       sporreundersokelse.pabegynt,
-                       sporreundersokelse.fullfort,
-                       sporreundersokelse_tema.tema_id,
-                       sporreundersokelse_tema.stengt                        AS tema_stengt,
-                       tema.navn                                             AS tema_navn,
-                       tema.status                                           AS tema_status,
-                       tema.rekkefolge                                       AS tema_rekkefolge,
-                       tema.sist_endret                                      AS tema_endret,
-                       undertema.undertema_id,
-                       undertema.navn                                        AS undertema_navn,
-                       undertema.status                                      AS undertema_status,
-                       undertema.rekkefolge                                  AS undertema_rekkefolge,
-                       undertema.sist_endret                                 AS undertema_endret,
-                       sporsmal.sporsmal_id,
-                       sporsmal.sporsmal_tekst,
-                       sporsmal_undertema.rekkefolge                         AS sporsmal_rekkefolge,
-                       sporsmal.flervalg,
-                       svaralternativ.svaralternativ_id,
-                       svaralternativ.svaralternativ_tekst,
-                       dokument_til_publisering.status                       AS publisering_status,
-                       COALESCE(
-                               (SELECT CASE
-                                           WHEN (SELECT COUNT(DISTINCT us.sesjon_id)
-                                                 FROM ia_sak_kartlegging_svar us
-                                                 WHERE us.kartlegging_id = sporreundersokelse.kartlegging_id
-                                                   AND us.sporsmal_id = sporsmal.sporsmal_id) < 3
-                                               THEN 0
-                                           ELSE COUNT(DISTINCT svar.sesjon_id)
-                                           END
-                                FROM ia_sak_kartlegging_svar svar
-                                WHERE svar.kartlegging_id = sporreundersokelse.kartlegging_id
-                                  AND svar.sporsmal_id = sporsmal.sporsmal_id
-                                  AND svar.svar_ider @> ('["' || svaralternativ.svaralternativ_id || '"]')::jsonb), 0
-                       )                                                     AS antall_svar,
-                       (SELECT COUNT(DISTINCT unike_svar.sesjon_id)
-                        FROM ia_sak_kartlegging_svar unike_svar
-                        WHERE unike_svar.kartlegging_id = sporreundersokelse.kartlegging_id
-                          AND unike_svar.sporsmal_id = sporsmal.sporsmal_id) AS antall_svar_per_sporsmal
-                FROM ia_sak_kartlegging sporreundersokelse
-                         JOIN ia_prosess samarbeid
-                              ON sporreundersokelse.ia_prosess = samarbeid.id
-                         JOIN ia_sak sak USING (saksnummer, orgnr)
-                         JOIN virksomhet USING (orgnr)
-                         JOIN ia_sak_kartlegging_kartlegging_til_undertema sporreundersokelse_undertema
-                              ON sporreundersokelse.kartlegging_id = sporreundersokelse_undertema.kartlegging_id
-                         JOIN ia_sak_kartlegging_undertema undertema
-                              ON sporreundersokelse_undertema.undertema_id = undertema.undertema_id
-                         JOIN ia_sak_kartlegging_sporsmal_til_undertema sporsmal_undertema
-                              ON undertema.undertema_id = sporsmal_undertema.undertema_id
-                         JOIN ia_sak_kartlegging_sporsmal sporsmal
-                              ON sporsmal_undertema.sporsmal_id = sporsmal.sporsmal_id
-                         LEFT JOIN ia_sak_kartlegging_svaralternativer svaralternativ
-                                   ON sporsmal.sporsmal_id = svaralternativ.sporsmal_id
-                         LEFT JOIN ia_sak_kartlegging_kartlegging_til_tema sporreundersokelse_tema
-                                   ON sporreundersokelse_tema.kartlegging_id = sporreundersokelse.kartlegging_id
-                                       AND sporreundersokelse_tema.tema_id = undertema.tema_id
-                         LEFT JOIN ia_sak_kartlegging_tema tema
-                                   ON tema.tema_id = sporreundersokelse_tema.tema_id
-                         LEFT JOIN dokument_til_publisering
-                                   ON dokument_til_publisering.referanse_id = sporreundersokelse.kartlegging_id
-                WHERE sporreundersokelse.kartlegging_id = :kartleggingId
+                    SELECT virksomhet.navn,
+                           virksomhet.orgnr,
+                           sak.saksnummer,
+                           samarbeid.id                                          AS samarbeid_id,
+                           sporreundersokelse.kartlegging_id                     AS sporreundersokelse_id,
+                           sporreundersokelse.type                               AS type,
+                           sporreundersokelse.status                             AS status,
+                           sporreundersokelse.opprettet_av,
+                           sporreundersokelse.opprettet,
+                           sporreundersokelse.gyldig_til,
+                           sporreundersokelse.endret,
+                           sporreundersokelse.pabegynt,
+                           sporreundersokelse.fullfort,
+                           sporreundersokelse_tema.tema_id,
+                           sporreundersokelse_tema.stengt                        AS tema_stengt,
+                           tema.navn                                             AS tema_navn,
+                           tema.status                                           AS tema_status,
+                           tema.rekkefolge                                       AS tema_rekkefolge,
+                           tema.sist_endret                                      AS tema_endret,
+                           undertema.undertema_id,
+                           undertema.navn                                        AS undertema_navn,
+                           undertema.status                                      AS undertema_status,
+                           undertema.rekkefolge                                  AS undertema_rekkefolge,
+                           undertema.sist_endret                                 AS undertema_endret,
+                           sporsmal.sporsmal_id,
+                           sporsmal.sporsmal_tekst,
+                           sporsmal_undertema.rekkefolge                         AS sporsmal_rekkefolge,
+                           sporsmal.flervalg,
+                           svaralternativ.svaralternativ_id,
+                           svaralternativ.svaralternativ_tekst,
+                           dokument_til_publisering.status                       AS publisering_status,
+                           COALESCE(
+                                   (SELECT CASE
+                                               WHEN (SELECT COUNT(DISTINCT us.sesjon_id)
+                                                     FROM ia_sak_kartlegging_svar us
+                                                     WHERE us.kartlegging_id = sporreundersokelse.kartlegging_id
+                                                       AND us.sporsmal_id = sporsmal.sporsmal_id) < $MINIMUM_ANTALL_SVAR_FØR_MASKERING
+                                                   THEN 0
+                                               ELSE COUNT(DISTINCT svar.sesjon_id)
+                                               END
+                                    FROM ia_sak_kartlegging_svar svar
+                                    WHERE svar.kartlegging_id = sporreundersokelse.kartlegging_id
+                                      AND svar.sporsmal_id = sporsmal.sporsmal_id
+                                      AND svar.svar_ider @> ('["' || svaralternativ.svaralternativ_id || '"]')::jsonb), 0
+                           )                                                     AS antall_svar,
+                           (SELECT COUNT(DISTINCT unike_svar.sesjon_id)
+                            FROM ia_sak_kartlegging_svar unike_svar
+                            WHERE unike_svar.kartlegging_id = sporreundersokelse.kartlegging_id
+                              AND unike_svar.sporsmal_id = sporsmal.sporsmal_id) AS antall_svar_per_sporsmal
+                    FROM ia_sak_kartlegging sporreundersokelse
+                             JOIN ia_prosess samarbeid
+                                  ON sporreundersokelse.ia_prosess = samarbeid.id
+                             JOIN ia_sak sak USING (saksnummer, orgnr)
+                             JOIN virksomhet USING (orgnr)
+                             JOIN ia_sak_kartlegging_kartlegging_til_undertema sporreundersokelse_undertema
+                                  ON sporreundersokelse.kartlegging_id = sporreundersokelse_undertema.kartlegging_id
+                             JOIN ia_sak_kartlegging_undertema undertema
+                                  ON sporreundersokelse_undertema.undertema_id = undertema.undertema_id
+                             JOIN ia_sak_kartlegging_sporsmal_til_undertema sporsmal_undertema
+                                  ON undertema.undertema_id = sporsmal_undertema.undertema_id
+                             JOIN ia_sak_kartlegging_sporsmal sporsmal
+                                  ON sporsmal_undertema.sporsmal_id = sporsmal.sporsmal_id
+                             LEFT JOIN ia_sak_kartlegging_svaralternativer svaralternativ
+                                       ON sporsmal.sporsmal_id = svaralternativ.sporsmal_id
+                             LEFT JOIN ia_sak_kartlegging_kartlegging_til_tema sporreundersokelse_tema
+                                       ON sporreundersokelse_tema.kartlegging_id = sporreundersokelse.kartlegging_id
+                                           AND sporreundersokelse_tema.tema_id = undertema.tema_id
+                             LEFT JOIN ia_sak_kartlegging_tema tema
+                                       ON tema.tema_id = sporreundersokelse_tema.tema_id
+                             LEFT JOIN dokument_til_publisering
+                                       ON dokument_til_publisering.referanse_id = sporreundersokelse.kartlegging_id
+                    WHERE sporreundersokelse.kartlegging_id = :kartleggingId
                     """.trimMargin(),
                     mapOf(
                         "kartleggingId" to spørreundersøkelseId.toString(),
@@ -603,7 +607,7 @@ class SpørreundersøkelseRepository(
                                            WHEN (SELECT COUNT(DISTINCT us.sesjon_id)
                                                  FROM ia_sak_kartlegging_svar us
                                                  WHERE us.kartlegging_id = sporreundersokelse.kartlegging_id
-                                                   AND us.sporsmal_id = sporsmal.sporsmal_id) < 3
+                                                   AND us.sporsmal_id = sporsmal.sporsmal_id) < $MINIMUM_ANTALL_SVAR_FØR_MASKERING
                                                THEN 0
                                            ELSE COUNT(DISTINCT svar.sesjon_id)
                                            END
