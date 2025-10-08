54b2f4377af9e4dd3d1c2ed4270f3df613c534e6
Thomas Dufourd <thomas.dufourd@gmail.com>
2025-10-08 09:57:39 +0200

    Legg til sistPublisert og publiseringStatus i response til GET plan
    Co-authored-by: Christian Klem <christian.klem@nav.no>
    Co-authored-by: Brage Hagemann Brataas <Brage.Hagemann.Brataas@nav.no>

--- src/main/kotlin/no/nav/lydia/ia/sak/api/plan/PlanDto.kt
+++ src/main/kotlin/no/nav/lydia/ia/sak/api/plan/PlanDto.kt
@@ -7,23 +7,49 @@ import no.nav.lydia.ia.sak.api.dokument.DokumentPublisering
 import no.nav.lydia.ia.sak.domene.plan.Plan
 import no.nav.lydia.ia.sak.domene.samarbeid.IASamarbeid
 
+interface PlanDtoI {
+    val id: String
+    val sistEndret: LocalDateTime
+    val status: IASamarbeid.Status
+    val temaer: List<PlanTemaDto>
+}
+
 @Serializable
 data class PlanDto(
-    val id: String,
-    val sistEndret: LocalDateTime,
+    override val id: String,
+    override val sistEndret: LocalDateTime,
     val sistPublisert: LocalDate?,
-    val status: IASamarbeid.Status,
-    val temaer: List<PlanTemaDto>,
-    val publiseringStatus: DokumentPublisering.Status? = null,
-)
+    override val status: IASamarbeid.Status,
+    override val temaer: List<PlanTemaDto>,
+) : PlanDtoI
 
-fun Plan.tilDto(): PlanDto = this.tilDto(publiseringStatus = null)
-
-fun Plan.tilDto(publiseringStatus: DokumentPublisering.Status?): PlanDto =
+fun Plan.tilDto(): PlanDto =
     PlanDto(
         id = id.toString(),
         sistEndret = sistEndret,
         sistPublisert = sistPublisert,
+        status = status,
+        temaer = temaer.tilDtoer(),
+    )
+
+@Serializable
+data class PlanMedPubliseringStatusDto(
+    override val id: String,
+    override val sistEndret: LocalDateTime,
+    override val status: IASamarbeid.Status,
+    override val temaer: List<PlanTemaDto>,
+    val sistPublisert: LocalDateTime? = null,
+    val publiseringStatus: DokumentPublisering.Status? = null,
+) : PlanDtoI
+
+fun Plan.tilDtoMedPubliseringStatus(
+    publiseringStatus: DokumentPublisering.Status? = null,
+    publiseringTidspunkt: LocalDateTime? = null,
+): PlanMedPubliseringStatusDto =
+    PlanMedPubliseringStatusDto(
+        id = id.toString(),
+        sistEndret = sistEndret,
+        sistPublisert = publiseringTidspunkt,
         status = status,
         temaer = temaer.tilDtoer(),
         publiseringStatus = publiseringStatus,
