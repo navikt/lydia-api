e3b01cfac91d950cd8b51368f3dc06f46d20d81c
Christian Klem <christian.klem@nav.no>
2025-10-08 15:31:38 +0200

    Håndter lagring av kvittering flere ganger for samme plan
    Co-authored-by: Thomas Dufourd <thomas.dufourd@nav.no>
    Co-authored-by: Brage Hagemann Brataas <Brage.Hagemann.Brataas@nav.no>

--- src/test/kotlin/no/nav/lydia/container/integrasjoner/kvittering/KvitteringConsumerTest.kt
+++ src/test/kotlin/no/nav/lydia/container/integrasjoner/kvittering/KvitteringConsumerTest.kt
@@ -1,12 +1,17 @@
 package no.nav.lydia.container.integrasjoner.kvittering
 
+import io.kotest.inspectors.forAtLeastOne
 import io.kotest.matchers.shouldBe
 import kotlinx.datetime.toKotlinLocalDateTime
 import kotlinx.serialization.json.Json
 import no.nav.lydia.Topic
 import no.nav.lydia.helper.DokumentPubliseringHelper.Companion.hentDokumentPublisering
 import no.nav.lydia.helper.DokumentPubliseringHelper.Companion.publiserDokument
+import no.nav.lydia.helper.DokumentPubliseringHelper.Companion.sendKvittering
 import no.nav.lydia.helper.IASakSpørreundersøkelseHelper.Companion.opprettSvarOgAvsluttSpørreundersøkelse
+import no.nav.lydia.helper.PlanHelper.Companion.hentPlanMal
+import no.nav.lydia.helper.PlanHelper.Companion.inkluderAlt
+import no.nav.lydia.helper.PlanHelper.Companion.opprettEnPlan
 import no.nav.lydia.helper.SakHelper.Companion.nySakIKartleggesMedEtSamarbeid
 import no.nav.lydia.helper.TestContainerHelper
 import no.nav.lydia.helper.TestContainerHelper.Companion.authContainerHelper
@@ -43,6 +48,44 @@ class KvitteringConsumerTest {
         val dokumentEtterPublisering = hentDokumentPublisering(dokument.referanseId)
         dokumentEtterPublisering.dokumentId shouldBe kvittering.dokumentId
         dokumentEtterPublisering.status shouldBe DokumentPublisering.Status.PUBLISERT
+    }
+
+    @Test
+    fun `skal kunne publisere samarbeidsplan flere ganger`() {
+        val sak = nySakIKartleggesMedEtSamarbeid()
+        val plan = sak.opprettEnPlan(plan = hentPlanMal().inkluderAlt())
+
+        val dokument = publiserDokument(
+            dokumentReferanseId = plan.id,
+            dokumentType = DokumentPublisering.Type.SAMARBEIDSPLAN,
+            token = authContainerHelper.saksbehandler1.token,
+        ).third.get()
+        sendKvittering(dokument)
+
+        val dokumentEtterPublisering = hentDokumentPublisering(
+            dokumentReferanseId = dokument.referanseId,
+            dokumentType = DokumentPublisering.Type.SAMARBEIDSPLAN,
+        )
+        dokumentEtterPublisering.status shouldBe DokumentPublisering.Status.PUBLISERT
+
+        val dokumentV2 = publiserDokument(
+            dokumentReferanseId = plan.id,
+            dokumentType = DokumentPublisering.Type.SAMARBEIDSPLAN,
+            token = authContainerHelper.saksbehandler1.token,
+        ).third.get()
+        sendKvittering(dokumentV2)
+
+        val dokumentEtterRePublisering = hentDokumentPublisering(
+            dokumentReferanseId = dokumentV2.referanseId,
+            dokumentType = DokumentPublisering.Type.SAMARBEIDSPLAN,
+        )
+        dokumentEtterRePublisering.status shouldBe DokumentPublisering.Status.PUBLISERT
+
+        val dokumenter = TestContainerHelper.postgresContainerHelper.hentAlleRaderTilEnkelKolonne<String>(
+            "select dokument_id from dokument_til_publisering where referanse_id = '${plan.id}'",
+        )
+        dokumenter.forAtLeastOne { it shouldBe dokumentEtterPublisering.dokumentId }
+        dokumenter.forAtLeastOne { it shouldBe dokumentEtterRePublisering.dokumentId }
     }
 }
 
