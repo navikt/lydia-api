ff5541c48601404431b84354737ec8d8956f275a
Thomas Dufourd <thomas.dufourd@gmail.com>
2025-11-28 11:56:22 +0100

    Oppdater DB dump av test data etter publisering av 3. kvartal i 2025

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 ./gradlew build -x test
 docker-compose up -d
-curl -o db_script.sql https://raw.githubusercontent.com/navikt/lydia-api/main/scripts/db/lydia-api-container-db_localhost-2025_09_08_08_06_05-dump.sql
+curl -o db_script.sql https://raw.githubusercontent.com/navikt/lydia-api/main/scripts/db/lydia-api-container-db_localhost-2025_11_28_11_48_56-dump.sql
 sleep 3
 PGPASSWORD=test psql -h localhost -p 5432 -U postgres -f db_script.sql > /dev/null
 rm db_script.sql
