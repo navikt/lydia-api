774d10cc18ad11d56f6a0b8c1cdb8c6d9156a309
Christian Klem <christian.klem@nav.no>
2026-01-16 15:47:55 +0100

    Oppdater avhengigheter

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
-val ktorVersion = "3.3.1"
+val ktorVersion = "3.3.3"
 val fuelVersion = "2.3.1"
 val iaFellesVersion = "2.0.4"
-val kotestVerstion = "6.0.4"
-val testcontainersVersion = "2.0.1"
-val logbackVersion = "1.5.20"
+val kotestVerstion = "6.0.7"
+val testcontainersVersion = "2.0.3"
+val logbackVersion = "1.5.24"
 val logstashLogbackEncoderVersion = "9.0"
 val opentelemetryLogbackMdcVersion = "2.16.0-alpha"
 
@@ -50,12 +50,12 @@ dependencies {
 
     // metrics
     implementation("io.ktor:ktor-server-metrics-micrometer:$ktorVersion")
-    implementation("io.micrometer:micrometer-registry-prometheus:1.15.5")
+    implementation("io.micrometer:micrometer-registry-prometheus:1.16.2")
 
     // Database
-    implementation("org.postgresql:postgresql:42.7.8")
+    implementation("org.postgresql:postgresql:42.7.9")
     implementation("com.zaxxer:HikariCP:7.0.2")
-    implementation("org.flywaydb:flyway-database-postgresql:11.15.0")
+    implementation("org.flywaydb:flyway-database-postgresql:11.20.2")
     implementation("com.github.seratch:kotliquery:1.9.1")
 
     // Enklere httpklient
@@ -63,8 +63,8 @@ dependencies {
     implementation("com.google.code.gson:gson:2.13.2")
 
     // Kafka
-    implementation("at.yawk.lz4:lz4-java:1.10.1")
-    implementation("org.apache.kafka:kafka-clients:4.1.0") {
+    implementation("at.yawk.lz4:lz4-java:1.10.2")
+    implementation("org.apache.kafka:kafka-clients:4.1.1") {
         // "Fikser CVE-2025-12183 - lz4-java >1.8.1 har sårbar versjon (transitive dependency fra kafka-clients:4.1.0)"
         exclude("org.lz4", "lz4-java")
     }
@@ -73,7 +73,7 @@ dependencies {
     implementation("com.github.guepardoapps:kulid:2.0.0.0")
 
     // Funksjonelle operatorer
-    implementation("io.arrow-kt:arrow-core:2.2.0")
+    implementation("io.arrow-kt:arrow-core:2.2.1")
 
     // audit log
     implementation("com.papertrailapp:logback-syslog4j:1.0.0")
@@ -83,7 +83,7 @@ dependencies {
 
     implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.2")
 
-    implementation("com.nimbusds:nimbus-jose-jwt:10.5")
+    implementation("com.nimbusds:nimbus-jose-jwt:10.7")
 
     // TEST
     testImplementation("org.jetbrains.kotlin:kotlin-test")
@@ -100,18 +100,26 @@ dependencies {
 
     // Http-mocking
     testImplementation("com.github.kittinunf.fuel:fuel-kotlinx-serialization:$fuelVersion")
-    testImplementation("org.wiremock:wiremock-standalone:3.13.1")
+    testImplementation("org.wiremock:wiremock-standalone:3.13.2")
 
     // -- validere pdfa
     testImplementation("org.verapdf:validation-model:1.28.2")
 
     // Autentisering
-    testImplementation("no.nav.security:mock-oauth2-server:3.0.0")
+    testImplementation("no.nav.security:mock-oauth2-server:3.0.1")
 
     constraints {
+        implementation("org.mozilla:rhino") {
+            version {
+                require("1.8.1")
+            }
+            because(
+                "versjonser < 1.8.1 har sårbarhet. inkludert i verapdf 1.28.2",
+            )
+        }
         implementation("io.netty:netty-codec-http2") {
             version {
-                require("4.2.7.Final")
+                require("4.2.9.Final")
             }
             because(
                 "ktor-server-netty har sårbar versjon",
