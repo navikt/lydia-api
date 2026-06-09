a6ecdffeae53e0abf43f7063d0b8214518fc4a79
Ida Sandberg Motzfeldt <ida.sandberg.motzfeldt@nav.no>
2026-06-09 10:01:10 +0200

    Delete run.sh

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-./gradlew build -x test
-docker-compose up -d
-curl -o db_script.sql https://raw.githubusercontent.com/navikt/lydia-api/main/scripts/db/lydia-api-container-db_localhost-2026_06_01_16_28_20-dump.sql
-sleep 3
-PGPASSWORD=test psql -h localhost -p 5432 -U postgres -f db_script.sql > /dev/null
-rm db_script.sql
