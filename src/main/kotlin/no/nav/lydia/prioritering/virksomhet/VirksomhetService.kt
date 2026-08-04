6bf1f3baea32575bc4c2a90f8d0dea99f353a2ea
Ida Sandberg Motzfeldt <ida.sandberg.motzfeldt@nav.no>
2026-08-04 15:20:16 +0200

    Revert "Legg til temp-endepunkt for å behandle tidligere slettede virksomheter"
    This reverts commit d344d41f525b9b53e0da14288e7838fe9f93e67c.

--- src/main/kotlin/no/nav/lydia/prioritering/virksomhet/VirksomhetService.kt
+++ src/main/kotlin/no/nav/lydia/prioritering/virksomhet/VirksomhetService.kt
@@ -21,7 +21,6 @@ import no.nav.lydia.tilstandsmaskin.hendelse.Avregistrering
 import no.nav.lydia.tilstandsmaskin.hendelse.VirksomhetErAvregistrertIBrreg
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import kotlin.time.Clock
 
 class VirksomhetService(
     val virksomhetRepository: VirksomhetRepository,
@@ -76,23 +75,6 @@ class VirksomhetService(
 
                 null -> {}
             }
-        }
-
-    fun tempBehandleSlettedeVirksomheter(): Either<Feil, String> =
-        either {
-            val virksomheter = virksomhetRepository.tempFinnSlettedeVirksomheterMedFeilStatus()
-            virksomheter.forEach { (orgnr, endringstype) ->
-                oppdaterStatusTilVirksomhetTilSlettetEllerFjernet(
-                    BrregOppdateringConsumer.OppdateringVirksomhet(
-                        orgnummer = orgnr,
-                        oppdateringsid = 0L,
-                        endringstype = endringstype,
-                        metadata = null,
-                        endringstidspunkt = Clock.System.now(),
-                    ),
-                ).bind()
-            }
-            "Vellykket oppdatering av ${virksomheter.size} slettede/fjernede virksomheter."
         }
 
     private fun hentAvregistreringstype(oppdatering: BrregOppdateringConsumer.BrregVirksomhetEndringstype) =
