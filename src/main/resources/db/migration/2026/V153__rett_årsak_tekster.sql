9e0ed96f34712d91a28dce282d4543c9f97a5ac0
Ida Sandberg Motzfeldt <ida.sandberg.motzfeldt@nav.no>
2026-09-08 09:05:13 +0200

    Rett opp feil i aarsak_enum

--- src/main/resources/db/migration/2026/V153__rett_årsak_tekster.sql
+++ src/main/resources/db/migration/2026/V153__rett_årsak_tekster.sql
@@ -4,7 +4,7 @@ WHERE aarsak_enum = 'VIRKSOMHETEN_VURDERES_PÅ_ET_SENERE_TIDSPUNKT';
 
 UPDATE hendelse_begrunnelse
 SET aarsak = 'Nav har konkludert'
-WHERE aarsak_enum = ' "Nav har konkludert"';
+WHERE aarsak_enum = 'VIRKSOMHETEN_ER_FERDIG_VURDERT_MED_INTERN_VURDERING';
 
 UPDATE hendelse_begrunnelse
 SET aarsak = 'Virksomheten har takket nei'
